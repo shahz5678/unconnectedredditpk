fde25037be067009e66b86f87b639cb70386090b
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-02 00:49:43 +0300

    added googleanalyticsmiddleware

--- unconnectedreddit/settings.py
+++ unconnectedreddit/settings.py
@@ -107,7 +107,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    'google_analytics.middleware.GoogleAnalyticsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

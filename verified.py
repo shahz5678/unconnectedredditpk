5edf95e8a6c246ee4593858e1a276d43d881abc0
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-13 04:22:55 +0300

    added gori as local environment tester

--- verified.py
+++ verified.py
@@ -1,1 +1,1 @@
-FEMALES = ['samreen', 'Farheen', 'pholi', 'AlinaNoor_786', 'rabika', 'mehro', 'Sheeery', 'Maleenjalee','Rimiii','Remsha','sherikhokhar5','Roshni_malik_53','Rahat',]
+FEMALES = ['gori', 'samreen', 'Farheen', 'pholi', 'AlinaNoor_786', 'rabika', 'mehro', 'Sheeery', 'Maleenjalee','Rimiii','Remsha','sherikhokhar5','Roshni_malik_53','Rahat',]

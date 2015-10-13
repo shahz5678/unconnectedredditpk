fee54925490ba53f3a5fdf2532958b98ddcca108
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-13 04:29:15 +0300

    added Publicreply model to admin panel

--- links/admin.py
+++ links/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Link, Vote, UserProfile, UserSettings
+from .models import Link, Vote, UserProfile, UserSettings, Publicreply
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 #these will appear in the admin panel
 class LinkAdmin(admin.ModelAdmin): pass
 admin.site.register(Link, LinkAdmin)
+
+class PublicreplyAdmin(admin.ModelAdmin): pass
+admin.site.register(Publicreply, PublicreplyAdmin)
 
 class VoteAdmin(admin.ModelAdmin): pass
 admin.site.register(Vote, VoteAdmin)

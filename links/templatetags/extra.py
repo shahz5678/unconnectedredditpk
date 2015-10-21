8e560e8c3b3078a5ff52a81d166dd61289185eaf
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-21 07:07:10 +0300

    template tag

---
+++ links/templatetags/extra.py
@@ -0,0 +1,12 @@
+from django import template
+from django.template import Variable, VariableDoesNotExist
+register = template.Library()
+
+@register.filter
+def hash(object,attr):
+	pseudo_context={'object':object}
+	try:
+		value = Variable('object.%s' % attr).resolve(pseudo_context)
+	except VariableDoesNotExist:
+		value = None
+	return value

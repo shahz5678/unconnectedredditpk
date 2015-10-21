1055c099d549b56bb962d754f7bd4f33b89f2ff3
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-21 07:03:09 +0300

    added new models to admin

--- links/admin.py
+++ links/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Link, Vote, UserProfile, UserSettings, Publicreply
+from .models import Link, Vote, UserProfile, UserSettings, Publicreply, Seen, Unseennotification
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 #these will appear in the admin panel
 class LinkAdmin(admin.ModelAdmin): pass
 admin.site.register(Link, LinkAdmin)
 
+class UnseennotificationAdmin(admin.ModelAdmin): pass
+admin.site.register(Unseennotification, UnseennotificationAdmin)
+
 class PublicreplyAdmin(admin.ModelAdmin): pass
 admin.site.register(Publicreply, PublicreplyAdmin)
+
+class SeenAdmin(admin.ModelAdmin): pass
+admin.site.register(Seen, SeenAdmin)
 
 class VoteAdmin(admin.ModelAdmin): pass
 admin.site.register(Vote, VoteAdmin)

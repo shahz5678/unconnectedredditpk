f5cc8bdbf5784c85c1e4aca4295b6cfbf1258866
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-10-21 07:17:41 +0300

    added unseenactivity and history help url pattern

--- unconnectedreddit/urls.py
+++ unconnectedreddit/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
 from links.models import UserProfile, Vote
 from django.views.generic.base import TemplateView
-from links.views import LinkListView, UserProfileDetailView, UserProfileEditView, LinkCreateView, LinkDetailView, LinkUpdateView, LinkDeleteView, VoteFormView, ScoreHelpView, UserSettingsEditView, HelpView, UserActivityView, WhoseOnlineView, RegisterHelpView, VerifyHelpView, PublicreplyView, ReportreplyView, ReportView #MyRegistrationView
+from links.views import LinkListView, UserProfileDetailView, UserProfileEditView, LinkCreateView, LinkDetailView, LinkUpdateView, LinkDeleteView, VoteFormView, ScoreHelpView, UserSettingsEditView, HelpView, UnseenActivityView, WhoseOnlineView, RegisterHelpView, VerifyHelpView, PublicreplyView, ReportreplyView, UserActivityView, ReportView, HistoryHelpView #MyRegistrationView
 
 
 admin.autodiscover()
@@ -23,9 +23,11 @@ urlpatterns = patterns('',
 	url(r'^accounts/', include('registration.backends.simple.urls')),
 	url(r'^online_kone/$', auth(WhoseOnlineView.as_view()), name='online_kone'),
 	url(r'^users/(?P<slug>[\w.@+-]+)/activity/$', auth(UserActivityView.as_view()), name="user_activity"),
+	url(r'^users/(?P<slug>[\w.@+-]+)/unseen/$', auth(UnseenActivityView.as_view()), name="unseen_activity"),
 	url(r'^link/create/$', LinkCreateView.as_view(), name='link_create'),
 	url(r'^link/(?P<pk>\d+)/$', LinkDetailView.as_view(), name='link_detail'),
 	url(r'^score/$', auth(ScoreHelpView.as_view()), name='score_help'),
+	url(r'^history/$', auth(HistoryHelpView.as_view()), name='history_help'),
 	url(r'^help/$', HelpView.as_view(), name='help'),
 	url(r'^register_help/$', RegisterHelpView.as_view(), name='register_help'),
 	url(r'^verify_help/$', VerifyHelpView.as_view(), name='verify_help'),

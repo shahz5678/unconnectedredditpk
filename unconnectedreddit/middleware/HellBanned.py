8a6c0defaf8ad592c9040a5150a1a7c8f6b82553
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-11-22 10:27:03 +0300

    middleware to calculate whether user is hellbanned or not

---
+++ unconnectedreddit/middleware/HellBanned.py
@@ -0,0 +1,11 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unconnectedreddit.settings")
+from links.models import HellBanList
+
+class HellBannedMiddleware(object):
+
+    def process_request(self, request):
+        if request.user.is_authenticated():
+        	request.user_banned = HellBanList.objects.filter(condemned=request.user).exists()
+    	else:
+    		request.user_banned = 0

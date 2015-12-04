000e9f7df6ea33817963c1ffa3f1d0e924c42f36
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-12-04 14:13:40 +0300

    two new photo uploaders

--- allowed.py
+++ allowed.py
@@ -1,1 +1,1 @@
-ALLOWED = ['Dilwale', 'Hafeeez34', 'Mehta', 'Mack_amr', 'Bashakhan', 'Zahid_Awan', 'Main_hoona_77', 'Azad.khanzada', 'Rounaq_ali', 'Janu', 'Tariq_Ali', 'Lover_boy', 'Sweet_boy', 'Faizan_Ali', 'NaeemRana','Badal','mhb11','AhmadMeher','Amjad_707','hari420',]
+ALLOWED = ['Dilwale', 'Hafeeez34', 'shahid72462', 'RiZz', 'Mehta', 'Mack_amr', 'Bashakhan', 'Zahid_Awan', 'Main_hoona_77', 'Azad.khanzada', 'Rounaq_ali', 'Janu', 'Tariq_Ali', 'Lover_boy', 'Sweet_boy', 'Faizan_Ali', 'NaeemRana','Badal','mhb11','AhmadMeher','Amjad_707','hari420',]

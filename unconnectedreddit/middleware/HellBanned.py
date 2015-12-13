a07c86e7f76389ba37d6263bc230b91a2e2f18eb
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-12-13 15:20:09 +0300

    print request.META

--- unconnectedreddit/middleware/HellBanned.py
+++ unconnectedreddit/middleware/HellBanned.py
@@ -5,6 +5,7 @@ from links.models import HellBanList
 class HellBannedMiddleware(object):
 
     def process_request(self, request):
+        print self.request.META
         if request.user.is_authenticated():
         	request.user_banned = HellBanList.objects.filter(condemned=request.user).exists()
     	else:

fc216e78f309760fa4c2f604b677b6c14b19082c
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-05-19 13:06:07 +0300

    changed celery.py

--- unconnectedreddit/celery.py
+++ unconnectedreddit/celery.py
@@ -7,7 +7,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unconnectedreddit.settings')
 
-app = Celery('links', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0',include=['unconnectedreddit.links.tasks'])
+app = Celery('links', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0',include=['unconnectedredditpk.links.tasks'])
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS) 
 

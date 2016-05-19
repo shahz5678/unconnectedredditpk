5e594a04baeed27c3dc0ac4315be75240542249f
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-05-19 13:05:26 +0300

    changed celery task

--- links/tasks.py
+++ links/tasks.py
@@ -1,13 +1,17 @@
 import os
 from unconnectedreddit import celery_app1
 import time
-from links.models import Link
+from links.models import Photo
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unconnectedreddit.settings")
 
-@celery_app1.task(name='tasks.rank_all')
-def rank_all():
-    for link in Link.with_votes.all():
-        link.set_rank()
-    
+# @celery_app1.task(name='tasks.rank_all')
+# def rank_all():
+#     for link in Link.with_votes.all():
+#         link.set_rank()
+
+@celery_app1.task(name='tasks.rank_all_photos')
+def rank_all_photos():
+	for photo in Photo.objects.all()[:400]:
+		photo.set_rank()
     

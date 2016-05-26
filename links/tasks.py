395111ee83c156773ec6746194a2a19d8816e9c6
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-05-26 21:54:11 +0300

    added new task

--- links/tasks.py
+++ links/tasks.py
@@ -1,7 +1,13 @@
 import os
 from unconnectedreddit import celery_app1
 import time
+from django.core.cache import get_cache, cache
+#from celery import shared_task
+from datetime import datetime, timedelta
+from django.utils import timezone
 from links.models import Photo
+from user_sessions.models import Session
+from django.contrib.auth.models import User
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unconnectedreddit.settings")
 
@@ -14,4 +20,14 @@ from links.models import Photo
 def rank_all_photos():
 	for photo in Photo.objects.order_by('-id')[:400]:
 		photo.set_rank()
-    
+
+#@shared_task(name='tasks.whoseonline')
+@celery_app1.task(name='tasks.whoseonline')
+def whoseonline():
+	unique_user_sessions = Session.objects.filter(last_activity__gte=(timezone.now()-timedelta(minutes=5))).only('user').distinct('user')
+	users = [session.user for session in unique_user_sessions]
+	users = [user for user in users if user is not None]
+	cache_mem = get_cache('django.core.cache.backends.memcached.MemcachedCache', **{
+            'LOCATION': '127.0.0.1:11211', 'TIMEOUT': 120,
+        })
+	cache_mem.set('online_users', users)  # expiring in 70 seconds

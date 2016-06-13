5ddb2361295fb83c4f752514fbd2f5e0901f5a2c
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-06-13 06:00:19 +0500

    notfication generation made asynchronous for photo comments and link publireplys

--- links/tasks.py
+++ links/tasks.py
@@ -6,7 +6,7 @@ from django.db.models import Count, Q
 import datetime
 from datetime import datetime, timedelta
 from django.utils import timezone
-from links.models import Photo, UserFan, PhotoObjectSubscription, LatestSalat
+from links.models import Photo, UserFan, PhotoObjectSubscription, LatestSalat, Photo, PhotoComment, Link, Publicreply
 from namaz_timings import namaz_timings, streak_alive
 from user_sessions.models import Session
 from django.contrib.auth.models import User
@@ -74,6 +74,50 @@ def salat_streaks():
     })
 	cache_mem.set('salat_streaks', object_list)  # expiring in 120 seconds
 
+@celery_app1.task(name='tasks.photo_tasks')
+def photo_tasks(user_id, photo_id, timestring, photocomment_id, count, text, it_exists):
+	user = User.objects.get(id=user_id)
+	photo = Photo.objects.get(id=photo_id)
+	timeobj = datetime.strptime(timestring, "%Y-%m-%dT%H:%M:%S.%f")
+	all_commenter_ids = list(set(PhotoComment.objects.filter(which_photo_id=photo_id).order_by('-id').values_list('submitted_by', flat=True)[:25]))
+	if photo.owner_id not in all_commenter_ids:	
+		all_commenter_ids.append(photo.owner_id)
+	PhotoObjectSubscription.objects.filter(viewer_id__in=all_commenter_ids, type_of_object='0', which_photo_id=photo_id).update(seen=False)			
+	exists = PhotoObjectSubscription.objects.filter(viewer_id=user_id, type_of_object='0', which_photo_id=photo_id).update(updated_at=timeobj, seen=True)
+	if not exists:
+		PhotoObjectSubscription.objects.create(viewer_id=user_id, which_photo_id=photo_id, type_of_object='0',updated_at=timeobj)
+	photo.second_latest_comment = photo.latest_comment
+	photo.latest_comment_id = photocomment_id
+	photo.comment_count = count
+	user.userprofile.previous_retort = text
+	if user_id != photo.owner_id and not id_exists:
+		user.userprofile.score = user.userprofile.score + 2 #giving score to the commenter
+		photo.owner.userprofile.media_score = photo.owner.userprofile.media_score + 2 #giving media score to the photo poster
+		photo.owner.userprofile.score = photo.owner.userprofile.score + 2 # giving score to the photo poster
+		photo.visible_score = photo.visible_score + 2
+		photo.owner.userprofile.save()
+	photo.save()
+	user.userprofile.save()
+
+@celery_app1.task(name='tasks.publicreply_tasks')
+def publicreply_tasks(user_id, answer_to_id, timestring, reply_id, count, description, score):
+	user = User.objects.get(id=user_id)
+	link = Link.objects.get(id=answer_to_id)
+	timeobj = datetime.strptime(timestring, "%Y-%m-%dT%H:%M:%S.%f")
+	all_reply_ids = list(set(Publicreply.objects.filter(answer_to_id=answer_to_id).order_by('-id').values_list('submitted_by', flat=True)[:25]))
+	if link.submitter_id not in all_reply_ids:
+		all_reply_ids.append(link.submitter_id)
+	PhotoObjectSubscription.objects.filter(viewer_id__in=all_reply_ids, type_of_object='2', which_link_id=answer_to_id).update(seen=False)
+	exists = PhotoObjectSubscription.objects.filter(viewer_id=user_id, type_of_object='2', which_link_id=answer_to_id).update(updated_at=timeobj, seen=True)
+	if not exists: #i.e. could not be updated
+		PhotoObjectSubscription.objects.create(viewer_id=user_id, type_of_object='2', which_link_id=answer_to_id, updated_at=timeobj)
+	link.latest_reply_id=reply_id
+	link.reply_count = count
+	link.save()
+	user.userprofile.previous_retort = description
+	user.userprofile.score = score
+	user.userprofile.save()
+	
 @celery_app1.task(name='tasks.bulk_create_notifications')
 def bulk_create_notifications(user_id, photo_id, timestring):
 	fans = UserFan.objects.filter(star_id=user_id).values_list('fan',flat=True)

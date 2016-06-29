fe13a4b8e020a4f48cc3e6980521468b5c96f5a7
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-06-29 13:18:01 +0500

    changed static url to point to CDN

--- unconnectedreddit/settings.py
+++ unconnectedreddit/settings.py
@@ -88,7 +88,7 @@ STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '//damadamstatic.azureedge.net/'#'/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

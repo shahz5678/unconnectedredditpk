2ccc4afa67ee2d0e3db78bd663ea230d76cfab67
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-06-30 00:49:55 +0500

    changing pattern matching

--- links/templatetags/thumbedge.py
+++ links/templatetags/thumbedge.py
@@ -5,13 +5,13 @@ register = template.Library()
 
 @register.filter(name='thumbedge')
 def cdnize_target_url(url):
-	if 'avatars' in url:
+	if 'res/avatars' in url:
 		url = string.replace(url, "damadam.blob.core.windows.net/pictures/avatars", "damadamthumbs.azureedge.net")
 		return url
-	elif 'users' in url:
+	elif 'res/users' in url:
 		url = string.replace(url, "damadam.blob.core.windows.net/pictures/users", "damadamthumbs.azureedge.net")
 		return url
-	elif 'photos' in url:
+	elif 'res/photos' in url:
 		url = string.replace(url, "damadam.blob.core.windows.net/pictures/photos", "damadamthumbs.azureedge.net")
 		return url
 	else:

e945cc92ca0bcc651ca68c5deaa2859fcfb93b86
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-07-05 17:03:37 +0500

    added CDN for mehfil images

--- links/templatetags/photoedge.py
+++ links/templatetags/photoedge.py
@@ -5,12 +5,12 @@ register = template.Library()
 
 @register.filter(name='photoedge')
 def cdnize_target_url(url):
-	if 'damadam.blob.core.windows.net' in url:
+	if 'damadam.blob.core.windows.net/pictures/photos' in url:
 		url = string.replace(url, "damadam.blob.core.windows.net/pictures/photos", "damadamphotos.azureedge.net")
 		return url
-	# elif 'users' in url:
-	# 	url = string.replace(url, "users", "thumbnails")
-	# 	return url
+	elif 'damadam.blob.core.windows.net/pictures/mehfils' in url:
+	 	url = string.replace(url, "damadam.blob.core.windows.net/pictures/mehfils", "damadammehfil.azureedge.net")
+	 	return url
 	# elif 'photos' in url:
 	# 	url = string.replace(url, "photos", "thumbnails")
 	# 	return url

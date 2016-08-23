d54bd2b63e716b943b01401573ea1de348694d6c
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-08-24 04:55:08 +0500

    initial commit

---
+++ links/azurevids/azurevids.py
@@ -0,0 +1,24 @@
+import subprocess
+def uploadvid(video):
+	#print video
+	proc = subprocess.Popen("php vodworkflow_drm_playready_widevine.php videos " \
+		+str(video), shell=True, stdout=subprocess.PIPE, cwd='/home/hassan/Desktop/unconnectedredditpk/links/azurevids/')
+	base_uri = ''
+	acc_comp = ''
+	list = ['b_64_000001.jpg',
+	'b_180_000001.jpg',
+	'b_720_000001.jpg',
+	'b_320x180_400.mp4',
+	'b_1280x720_3400.mp4' ]
+	while True:
+	  line = proc.stdout.readline()
+	  if line != '':
+	    #the real code does filtering here
+	    if line.startswith('BASE URI:'):
+	    	base_uri = line[10:].rstrip()
+	    if line.startswith('ACC COMP:'):
+	    	acc_comp = line[10:].rstrip()
+	    print line.rstrip()
+	  else:
+	    break
+	return [base_uri +'/'+s+acc_comp for s in list]
---
+++ links/azurevids/call_func.py
@@ -0,0 +1,2 @@
+import azurevids
+azurevids.uploadvid("8e1105a9-b085-4745-a2c2-72c826b46c6e.3gp")

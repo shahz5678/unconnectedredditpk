1974e531cb0d53052cdc0be91896d4fe1292d7b7
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-09-08 05:31:00 +0500

    minor tweak

--- links/views.py
+++ links/views.py
@@ -3519,10 +3519,7 @@ class PhotoView(ListView):
 
 	def get_queryset(self):
 		queryset = Photo.objects.select_related('owner__userprofile').filter(id__in=all_photos()).order_by('-id')
-		#queryset = Photo.objects.select_related('owner').order_by('-upload_time')[:200]
-		                         #select_related('owner__userprofile','latest_comment__submitted_by','second_latest_comment__submitted_by')
-		# Link.objects.select_related('submitter__userprofile','which_photostream__cover','submitter__hotuser')\
-		# 	.filter(id__in=all_unfiltered_posts()).order_by('-id')
+		#queryset = Photo.objects.select_related('owner__userprofile','latest_comment__submitted_by','second_latest_comment__submitted_by').order_by('-upload_time')[:200]
 		return queryset
 
 	def get_context_data(self, **kwargs):

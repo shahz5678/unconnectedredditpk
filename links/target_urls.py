34b80f053711c120ab98857c2654418b37b1a81e
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-12-23 02:27:15 +0000

    contains aasanads api urls to ping

---
+++ links/target_urls.py
@@ -0,0 +1,33 @@
+import json, requests
+from requests.auth import HTTPBasicAuth
+
+# DOMAIN = 'http://10.50.202.114:8000'
+DOMAIN = 'http://aasaninternet.com:786'
+
+SEND_SMS_URL = DOMAIN+'/api/ad/send_sms/'
+CREATE_URL = DOMAIN+'/api/ad/create/'
+EXPIRE_URL = DOMAIN+'/api/ad/update/'
+UPDATE_URL = DOMAIN+'/api/ad/update/'
+HISTORY_URL = DOMAIN+'/api/ad/history/'
+
+USERNAME = 'admin'
+PASSWORD = 'aasanads'
+
+CONNECT_TIMEOUT = 5#0.01
+READ_TIMEOUT = 5
+
+def call_aasan_api(data,operation):
+	data_json = json.dumps(data)
+	headers = {'Content-type':'application/json'}
+	if operation == 'expire':
+		target_url = EXPIRE_URL
+	elif operation == 'create':
+		target_url = CREATE_URL
+	elif operation == 'send_sms':
+		target_url = SEND_SMS_URL
+	elif operation == 'history':
+		target_url = HISTORY_URL
+	response = requests.post(target_url, data=data_json,auth=HTTPBasicAuth(USERNAME,PASSWORD), \
+		timeout=(CONNECT_TIMEOUT,READ_TIMEOUT))
+	return response
+	# print response.body

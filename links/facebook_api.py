475aee0a0be20129aaeca9fd58ebb2516f31a4a3
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-12-25 22:58:33 +0000

    calls made to facebook api from here

---
+++ links/facebook_api.py
@@ -0,0 +1,33 @@
+#full documentation at: http://nodotcom.org/python-facebook-tutorial.html
+
+import facebook, os
+
+def photo_poster(image_obj=None, caption=None):
+  # Fill in the values noted in documentation here:
+
+  PAGE_ID = os.environ.get('FACEBOOK_PAGE_ID')
+  ACCESS_TOKEN = os.environ.get('FACEBOOK_ACCESS_TOKEN')
+
+  cfg = {
+    "page_id"      : PAGE_ID,  # Step 1
+    "access_token" : ACCESS_TOKEN   # Step 3
+    }
+
+  api = get_api(cfg)
+  status = api.put_photo(image=(image_obj),message=caption)
+
+def get_api(cfg):
+  graph = facebook.GraphAPI(cfg['access_token'])
+  # Get page token to post as the page. You can skip 
+  # the following if you want to post as yourself. 
+  resp = graph.get_object('me/accounts')
+  # print resp
+  page_access_token = None
+  for page in resp['data']:
+    if page['id'] == cfg['page_id']:
+      page_access_token = page['access_token']
+  graph = facebook.GraphAPI(page_access_token)
+  return graph
+  # You can also skip the above if you get a page token:
+  # http://stackoverflow.com/questions/8231877/facebook-access-token-for-pages
+  # and make that long-lived token as in Step 3

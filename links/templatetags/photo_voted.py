dc545973457a5518cb36c8fed49e3bd0181e5038
Hassan Baig <hassan.baig@alumni.duke.edu>
2016-12-25 23:03:22 +0000

    tweaked this to fix an error

--- links/templatetags/photo_voted.py
+++ links/templatetags/photo_voted.py
@@ -1,6 +1,6 @@
 from django.template.defaulttags import register
-from links.redismodules import voted_for_photo
+from links.redis1 import voted_for_single_photo
 
 @register.filter(name='photo_voted')
 def photo_voted(photo_pk,username):
-	return voted_for_photo(photo_pk, username)
+	return voted_for_single_photo(photo_pk, username)

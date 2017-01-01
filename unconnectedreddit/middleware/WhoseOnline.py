3c2df85086e8f86cf37e5fef192a612af09b143c
Hassan Baig <hassan.baig@alumni.duke.edu>
2017-01-01 00:54:42 +0000

    added rand function

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -1,10 +1,10 @@
-import os
+import random
 from links.redis2 import set_whose_online
 
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
-    	if request.user.is_authenticated():
+    	if request.user.is_authenticated() and random.random() < 0.5:
     		set_whose_online(request.session['_auth_user_id'])
     	else:
     		pass

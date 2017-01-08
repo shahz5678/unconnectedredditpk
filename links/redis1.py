7963effd3941b292fde6ed66a7ccf3cfc68f0cab
Hassan Baig <hassan.baig@alumni.duke.edu>
2017-01-08 00:21:17 +0000

    commented a function

--- links/redis1.py
+++ links/redis1.py
@@ -450,25 +450,25 @@ def retrieve_home_links(link_id_list):
 		count += 1
 	return photo_ids, non_photo_link_ids, list_of_dictionaries 
 
-def retrieve_home_links2(link_id_list):
-	my_server = redis.Redis(connection_pool=POOL)
-	list_of_dictionaries = []
-	photo_ids = []
-	non_photo_link_ids = []
-	pipeline1 = my_server.pipeline()
-	for link_id in link_id_list:
-		hash_name = "lk:"+str(link_id)
-		pipeline1.hgetall(hash_name)#
-	result1 = pipeline1.execute()
-	count = 0
-	for hash_contents in result1:
-		list_of_dictionaries.append(hash_contents)
-		try:
-			photo_ids.append(hash_contents['pi'])
-		except:
-			non_photo_link_ids.append(link_id_list[count])
-		count += 1
-	return photo_ids, non_photo_link_ids, list_of_dictionaries
+# def retrieve_home_links2(link_id_list):
+# 	my_server = redis.Redis(connection_pool=POOL)
+# 	list_of_dictionaries = []
+# 	photo_ids = []
+# 	non_photo_link_ids = []
+# 	pipeline1 = my_server.pipeline()
+# 	for link_id in link_id_list:
+# 		hash_name = "lk:"+str(link_id)
+# 		pipeline1.hgetall(hash_name)#
+# 	result1 = pipeline1.execute()
+# 	count = 0
+# 	for hash_contents in result1:
+# 		list_of_dictionaries.append(hash_contents)
+# 		try:
+# 			photo_ids.append(hash_contents['pi'])
+# 		except:
+# 			non_photo_link_ids.append(link_id_list[count])
+# 		count += 1
+# 	return photo_ids, non_photo_link_ids, list_of_dictionaries
 
 def photo_link_mapping(photo_pk, link_pk):
 	my_server = redis.Redis(connection_pool=POOL)

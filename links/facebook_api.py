b4d05e1edd5dc51d41514a8dd95b989e5c380ad8
Hassan Baig <hassan.baig@alumni.duke.edu>
2017-01-09 02:49:20 +0000

    testing access token

--- links/facebook_api.py
+++ links/facebook_api.py
@@ -5,8 +5,8 @@ import facebook, os
 def photo_poster(image_obj=None, caption=None):
   # Fill in the values noted in documentation here:
 
-  PAGE_ID = os.environ.get('FACEBOOK_PAGE_ID')
-  ACCESS_TOKEN = os.environ.get('FACEBOOK_ACCESS_TOKEN')
+  PAGE_ID = '1812492382364506'#os.environ.get('FACEBOOK_PAGE_ID')
+  ACCESS_TOKEN = 'EAAQWkmy1MFYBAF4kDKRuQ8fjj2ocphX9vzijqY0SFbT5IfmRG9bvzfyamBMVGllsABmXORILAPH9qmYojcTNpZBHM8wx2ZCxOFMdOuZCRgOuqYixCrsVgbD4bhFoa8yxMGwISqPOBZAqviGQlcZB7induz6m8ZAfcZD'#os.environ.get('FACEBOOK_ACCESS_TOKEN')
 
   cfg = {
     "page_id"      : PAGE_ID,  # Step 1
@@ -23,8 +23,8 @@ def get_api(cfg):
   # print "graph: %s" % graph
   # Get page token to post as the page. You can skip 
   # the following if you want to post as yourself. 
-  # resp = graph.get_object('me/accounts')
-  resp = graph.get_object('10154886323073885/accounts')
+  resp = graph.get_object('me/accounts')
+  # resp = graph.get_object('10154886323073885/accounts')
   # print "getting own account: %s" % resp
   page_access_token = None
   for page in resp['data']:

c6496ed3a1d8bd67630b5d3942f0fecb5e2dffcd
mhb11 <hassan.baig@alumni.duke.edu>
2017-01-29 05:13:06 +0500

    slight optimization

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -4,7 +4,7 @@ from links.redis2 import set_whose_online
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
-    	if request.user.is_authenticated() and random.random() < 0.5:
+    	if request.user.is_authenticated() and random.random() < 0.4:
     		set_whose_online(request.session['_auth_user_id'])
     	else:
     		pass

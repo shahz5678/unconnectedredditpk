65c7b544ce79daec4b85ec929e744c5e62dfa6d1
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-01 17:36:30 +0500

    score file to universalize Dmdm scoring

---
+++ links/score.py
@@ -0,0 +1,5 @@
+PRIVATE_GROUP_COST = 500 # cost of buying a private group
+PUBLIC_GROUP_COST = 5000 # cost of buying a public group
+PUBLIC_GROUP_MESSAGE = 2 # score for messaging in public group
+PRIVATE_GROUP_MESSAGE = 1 # score for messaging in privte group
+PUBLICREPLY = 2 #score for leaving publicreply on a home 'link'

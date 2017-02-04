9108da6b14e252635e3766e1905ec56f4e8ab9d4
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-04 19:51:29 +0500

    catching exception

--- links/templatetags/future_time.py
+++ links/templatetags/future_time.py
@@ -3,11 +3,11 @@ import time
 
 @register.filter(name='future_time')
 def future_time(value):
-	m, s = divmod(value, 60)
-	h, m = divmod(m, 60)
-	if h:
-		return "%s hours baad" % int(h)
-	elif m:
-		return "%s minutes baad" % int(m)
-	elif s:
-		return "%s seconds baad" % int(s)
+	try:
+		m, s = divmod(value, 60)
+		if m:
+			return "%s minutes baad" % int(m)
+		elif s:
+			return "%s seconds baad" % int(s)
+	except:
+		return "kuch minute baad"

05a6381e490ae8226fcb372fbf5756a8d4ec76b4
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-06 04:42:59 +0500

    added form for BestPhotos

--- links/forms.py
+++ links/forms.py
@@ -421,6 +421,10 @@ class HomeLinkListForm(forms.Form):
 	class Meta:
 		fields = ("reply",)
 
+class BestPhotosListForm(forms.Form):
+	class Meta:
+		pass
+
 class UnauthHomeLinkListForm(forms.Form):
 	class Meta:
 		pass

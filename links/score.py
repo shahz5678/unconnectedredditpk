5a850ace1038f563ec6a03cdfb355dec82cd8a9f
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-09 14:40:23 +0500

    added new score constants

--- links/score.py
+++ links/score.py
@@ -3,3 +3,5 @@ PUBLIC_GROUP_COST = 5000 # cost of buying a public group
 PUBLIC_GROUP_MESSAGE = 2 # score for messaging in public group
 PRIVATE_GROUP_MESSAGE = 1 # score for messaging in privte group
 PUBLICREPLY = 2 #score for leaving publicreply on a home 'link'
+UPLOAD_PHOTO_REQ = 30 #score below which you're not allowed photo uploads
+PSL_SUPPORT_STARTING_POINT = 15 #score at which PSL support options open up

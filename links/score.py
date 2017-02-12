2f6c456413ab6009401eb17aba0396f551dff0f1
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-12 19:38:59 +0500

    added Cricket related constants

--- links/score.py
+++ links/score.py
@@ -4,4 +4,12 @@ PUBLIC_GROUP_MESSAGE = 2 # score for messaging in public group
 PRIVATE_GROUP_MESSAGE = 1 # score for messaging in privte group
 PUBLICREPLY = 2 #score for leaving publicreply on a home 'link'
 UPLOAD_PHOTO_REQ = 30 #score below which you're not allowed photo uploads
-PSL_SUPPORT_STARTING_POINT = 15 #score at which PSL support options open up
+CRICKET_SUPPORT_STARTING_POINT = 15 #score at which PSL support options open up
+CRICKET_TEAM_NAMES = {'Lahore Qalandars':'Qalandars','Islamabad United':'United',\
+'Karachi Kings':'Kings','Peshawar Zalmi':'Zalmi','Quetta Gladiators':'Gladiators',\
+'North':'North','Northwest':'Northwest'}
+CRICKET_TEAM_IDS = {'Lahore Qalandars':'5','Islamabad United':'8','Karachi Kings':'3',\
+'Peshawar Zalmi':'4','Quetta Gladiators':'7','misc':'9','North':'9','Northwest':'9'}
+CRICKET_COLOR_CLASSES = {'Lahore Qalandars':'qal','Islamabad United':'uni',\
+'Karachi Kings':'kng','Peshawar Zalmi':'zal','Quetta Gladiators':'gld','misc':'bcb',\
+'North':'bco','Northwest':'bco'}

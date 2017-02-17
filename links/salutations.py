dc00b0956ce4fd2aa9c39d90a2fdb7340611a043
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-17 09:08:26 +0500

    added variety of salutations for times when users are new

---
+++ links/salutations.py
@@ -0,0 +1,10 @@
+SALUTATIONS = ['damadam mast qalandar .b5','hello friends i am new .p1','Assalam o alikum dosto, im new .a1','Hello to all shining stars of Damadam. I am new .b5',\
+'Hy frnds kaisy hain aap!? Mein new hoon .o5','damadam ne boriat ko tabah o barbaad kr diya! .b1','My dear friends how are you all? Im new! .b5',\
+'tamam Damadam waloon ko mera salam .p5 im new','Damadam walo kaisey ho sb? Mein new hun .c4','Hello friends on Damadam. I am brand new .e1','I am brand new .l5',\
+'Hello Yellow Telloz .a5 Im N-E-W','mein ek zero meter Toyota Corolla ki terah new hun .p1','mein ek zero meter Honda ki terah new hun .p1','mein newest hun sb se ji .b5',\
+'apun damadam pe new hai .q1','damadam pe sb se new mein hoon .c1','mein hun new, aur tum sb ho old .c5 Hello uncle','A new user is in town ... ME .f5',\
+'I am shiny new on Damadam .d5','Assalam u alaikum to all Damadam .a1','mein cricket ki pehle over ki ball ki terah new hun .l5','Mein dabbay se nikle mobile ki terah new hoon .p1',\
+'im new please welcome me goodly .b1','.c1 apun new hai .c1 apun ko welcome .c1','I am new. Welcome nahi kro ge dosto? .j1','New hun, welcome kro na .k1','Im new please welcome me .j5',\
+'I am N-E-W on D-A-M-A-D-A-M .a1','Damadam pe new entry maar di mein ne .b5','Damadam walo, Assalam u alaikum sb ko .a1','oh teri, ye jagah kitni tezi se change ho rahi hai .i1',\
+'Damadam ke logo, sb ko mera salaam .p5','new logoon se dosti krne se sehat achi hoti hai .d5','mujh jaise new user ko welcome nah krna jurm hai .l5','mein hun ek neya parinda damadam ke ghosle mein .a1',\
+'mein new hun, aa kr sb haath milao .a5','Damadam zindabad, Pakistan zindabad .b5','shukr hai jhappi hai, puppy nahi .g5 chalo mujhe welcome kro','tm sb oldies ke beech mein im new .e5']

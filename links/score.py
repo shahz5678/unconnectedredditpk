617367d92e306ea0c04e5c206f16791f9129b7b5
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-18 07:03:26 +0500

    added score requirement for photos posted on home

--- links/score.py
+++ links/score.py
@@ -4,6 +4,7 @@ PUBLIC_GROUP_MESSAGE = 2 # score for messaging in public group
 PRIVATE_GROUP_MESSAGE = 1 # score for messaging in privte group
 PUBLICREPLY = 2 #score for leaving publicreply on a home 'link'
 UPLOAD_PHOTO_REQ = 30 #score below which you're not allowed photo uploads
+PHOTO_HOT_SCORE_REQ = 8 #aggregated 'visible_score' over previous 5 photos, above which your photo appears on home
 CRICKET_SUPPORT_STARTING_POINT = 15 #score at which PSL support options open up
 CRICKET_TEAM_NAMES = {'Lahore Qalandars':'Qalandars','Islamabad United':'United',\
 'Karachi Kings':'Kings','Peshawar Zalmi':'Zalmi','Quetta Gladiators':'Gladiators',\

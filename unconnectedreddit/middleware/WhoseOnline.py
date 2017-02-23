d0ca76502ccf9f36b90f98cfe6bc45963e321eab
mhb11 <hassan.baig@alumni.duke.edu>
2017-02-23 20:14:00 +0500

    added LUA functions to set whose online

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -1,10 +1,10 @@
 import random
-from links.redis2 import set_whose_online
+from links.redis2 import set_online_users#,set_whose_online
 
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
     	if request.user.is_authenticated() and random.random() < 0.4:
-    		set_whose_online(request.session['_auth_user_id'],request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
+    		set_online_users(request.session['_auth_user_id'],request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
     	else:
     		pass

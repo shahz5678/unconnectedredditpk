137116aedf48f8d7a0ee5c6e20a2c5badcbdb385
mhb11 <hassan.baig@alumni.duke.edu>
2017-03-01 18:25:01 +0500

    added South Africa and New Zealand cricket teams in add_home_link

--- links/redis1.py
+++ links/redis1.py
@@ -841,6 +841,14 @@ def add_home_link(link_pk=None, categ=None, nick=None, av_url=None, desc=None, \
 		# this is a link about misc
 		mapping = {'l':link_pk, 'c':categ, 'n':nick, 'au':av_url, 'de':desc, 'sc':scr, 'cc':cc, 'dc':device, 'w':writer_pk, \
 		't':time.time(),'ch':categ_head,'ct':categ_tail,'p':pinkstar,'rb':reply_button }
+	elif categ == '10':
+		# this is a link about New Zealand Cricket
+		mapping = {'l':link_pk, 'c':categ, 'n':nick, 'au':av_url, 'de':desc, 'sc':scr, 'cc':cc, 'dc':device, 'w':writer_pk, \
+		't':time.time(),'ch':categ_head,'ct':categ_tail,'p':pinkstar,'rb':reply_button }
+	elif categ == '11':
+		# this is a link about South African Cricket
+		mapping = {'l':link_pk, 'c':categ, 'n':nick, 'au':av_url, 'de':desc, 'sc':scr, 'cc':cc, 'dc':device, 'w':writer_pk, \
+		't':time.time(),'ch':categ_head,'ct':categ_tail,'p':pinkstar,'rb':reply_button }
 	# add the info in a hash
 	my_server.hmset(hash_name, mapping)
 

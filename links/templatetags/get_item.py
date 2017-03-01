e4ba0aab1218eb9f741068710ba2aae0b5faaf2a
mhb11 <hassan.baig@alumni.duke.edu>
2017-03-01 18:27:15 +0500

    added exception handling

--- links/templatetags/get_item.py
+++ links/templatetags/get_item.py
@@ -3,4 +3,7 @@ from django.template.defaulttags import register
 
 @register.filter(name='get_item')
 def get_item(dictionary, key):
-	return dictionary.get(key)
+	if dictionary:
+		return dictionary.get(key)
+	else:
+		return None

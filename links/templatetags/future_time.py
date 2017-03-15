fcba70d4eaca67e556ee11793f23162043794b3f
mhb11 <hassan.baig@alumni.duke.edu>
2017-03-15 11:27:43 +0500

    minor tweaks

--- links/templatetags/future_time.py
+++ links/templatetags/future_time.py
@@ -6,8 +6,8 @@ def future_time(value):
 	try:
 		m, s = divmod(value, 60)
 		if m:
-			return "%s minutes baad" % int(m)
+			return "%s min baad" % int(m)
 		elif s:
-			return "%s seconds baad" % int(s)
+			return "%s secs baad" % int(s)
 	except:
 		return "kuch waqt baad"

0720a92a4c52fcba9223c6b26b4b58fff96a9dd3
mhb11 <hassan.baig@alumni.duke.edu>
2017-03-17 17:24:08 +0500

    added unix socket for the third redis-server

--- links/location.py
+++ links/location.py
@@ -6,7 +6,9 @@ if ON_MAC == '1':
 	MEMLOC = 'unix:/usr/local/var/run/memcached/memcached.sock'
 	REDLOC1 = '/usr/local/var/run/redis/redis.sock'
 	REDLOC2 = '/usr/local/var/run/redis/redis2.sock' 
+	REDLOC3 = '/usr/local/var/run/redis/redis3.sock' 
 else:
 	MEMLOC = 'unix:/var/run/memcached/memcached.sock'
 	REDLOC1 = '/var/run/redis/redis.sock'
-	REDLOC2 = '/var/run/redis/redis2.sock' 
+	REDLOC2 = '/var/run/redis/redis2.sock'
+	REDLOC3 = '/var/run/redis/redis3.sock'

b95549d2b735e1be93ba28b0112d22c48fe45fff
mhb11 <hassan.baig@alumni.duke.edu>
2017-04-28 01:03:41 +0500

    tweaked

--- links/ecomm.py
+++ links/ecomm.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def show_ecomm_choices(request,*args,**kwargs):
 	return render(request,"ecomm_choices.html",{})
+
+def x32_details(request,*args,**kwargs):
+	return render(request,"x32_details.html",{})
+
+def x2lite_details(request,*args,**kwargs):
+	return render(request,"x2lite_details.html",{})	

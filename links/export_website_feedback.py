d5d270c8d5e8f45feecdd97c52cf447b47f3a78d
mhb11 <hassan.baig@alumni.duke.edu>
2017-05-31 04:09:07 +0500

    fixing minor error

--- links/export_website_feedback.py
+++ links/export_website_feedback.py
@@ -4,7 +4,10 @@ import csv
 
 def export_website_feedback(request,*args,**kwargs):
 	feedback = get_website_feedback()
-	keys = feedback[0].keys()
+	# keys = feedback[0].keys()
+	keys = ['username', 'answer5', 'answer4', 'answer1', 'answer3', 'answer2', \
+	'question5', 'question4', 'score', 'question1', 'question3', 'question2', \
+	'device', 'time_of_feedback', 'date_joined', 'city','gender','age','mobile']
 	with open('website_feedback.csv', 'wb') as output_file:
 	    dict_writer = csv.DictWriter(output_file, keys)
 	    dict_writer.writeheader()

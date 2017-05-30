922d4de61bc4d00fe88d6830f03b2fb24ccac9db
mhb11 <hassan.baig@alumni.duke.edu>
2017-05-30 19:05:55 +0500

    removed password error logging from create_password_new

--- links/views.py
+++ links/views.py
@@ -2777,13 +2777,13 @@ def create_password_new(request,slug=None,length=None,*args,**kwargs):
 				return render(request,'penalty_link_tinkered.html',{})
 		else:
 			###############################Logging Erroneous Password#####################################
-			try:
-				password = request.POST.get("password",None)
-				password = 'specificity' if password == '' else password
-				error_string = str(dict(form.errors)["password"]).split('<li>')[1].split('</li>')[0]
-				log_erroneous_passwords(password,error_string)
-			except:
-				pass
+			# try:
+			# 	password = request.POST.get("password",None)
+			# 	password = 'specificity' if password == '' else password
+			# 	error_string = str(dict(form.errors)["password"]).split('<li>')[1].split('</li>')[0]
+			# 	log_erroneous_passwords(password,error_string)
+			# except:
+			# 	pass
 			##############################################################################################
 			if int(length) == len(slug):
 				username = slug.decode("hex")

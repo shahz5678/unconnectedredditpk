26a0d20c3150477c3bc564b14d857085e7e52e07
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-04 03:35:14 +0500

    lessened random variable responsible for logging user request

--- links/redis4.py
+++ links/redis4.py
@@ -31,8 +31,12 @@ FIVE_MINS = 5*60
 #####################Retention Logger#####################
 def log_retention(server_instance, user_id):
 	time_now = time.time()
-	if time_now - float(server_instance.lrange("user_times:"+str(user_id),0,0)[0]) > TEN_MINS:
-		server_instance.lpush("user_times:"+str(user_id),time_now)
+	if server_instance.exists("user_times:"+user_id):
+		if time_now - float(server_instance.lrange("user_times:"+user_id,0,0)[0]) > TEN_MINS:
+			server_instance.lpush("user_times:"+user_id,time_now)
+			server_instance.sadd("logged_users",user_id)
+	else:
+		server_instance.lpush("user_times:"+user_id,time_now)
 		server_instance.sadd("logged_users",user_id)
 
 # def reduce_retention_data():
@@ -54,12 +58,13 @@ def expire_online_users():
 def set_online_users(user_id,user_ip):
 	my_server = redis.Redis(connection_pool=POOL)
 	sorted_set = "online_users"
-	latest_user_ip = "lip:"+str(user_id) #latest ip of user with 'user_id'
-	my_server.zadd(sorted_set,str(user_id)+":"+str(user_ip),time.time())
+	user_id = str(user_id)
+	latest_user_ip = "lip:"+user_id #latest ip of user with 'user_id'
+	my_server.zadd(sorted_set,user_id+":"+str(user_ip),time.time())
 	my_server.set(latest_user_ip,user_ip)
 	my_server.expire(latest_user_ip,FIVE_MINS)
-	# if random.random() < 0.5:
-	# 	log_retention(my_server,user_id)
+	if random.random() < 0.45:
+		log_retention(my_server,user_id)
 
 # invoked by tasks.py to show whoever is online in OnlineKon
 def get_recent_online():
@@ -67,13 +72,10 @@ def get_recent_online():
 	sorted_set = "online_users"
 	ten_mins_ago = time.time() - TEN_MINS
 	online_users = my_server.zrangebyscore(sorted_set,ten_mins_ago,'+inf')
-	# print online_users
 	online_ids = []
 	for user in online_users:
 		online_ids.append(user.split(":",1)[0])
-	# print online_ids
 	return online_ids
-	# return getlatestlogins(keys=[sorted_set],args=[time.time()])
 
 # invoked in views.py to show possible clones of users
 def get_clones(user_id):
--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -4,7 +4,7 @@ from links.redis4 import set_online_users
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
-    	if request.user.is_authenticated() and random.random() < 0.2:
+    	if request.user.is_authenticated() and random.random() < 0.15:
     		set_online_users(request.session['_auth_user_id'],request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
     	else:
     		pass

c7e87840774c17740686d1cf945dd629bc4ace30
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-04 00:39:29 +0500

    decreasing WhoseOnline logging to 20% of each request

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -4,7 +4,7 @@ from links.redis2 import set_online_users#,set_whose_online
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
-    	if request.user.is_authenticated() and random.random() < 0.4:
+    	if request.user.is_authenticated() and random.random() < 0.2:
     		set_online_users(request.session['_auth_user_id'],request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
     	else:
     		pass

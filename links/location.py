37f695422f6a0487d52ffcb93c88e97257ed66ef
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-04 01:31:52 +0500

    added redis4 sock file

--- links/location.py
+++ links/location.py
@@ -6,9 +6,11 @@ if ON_MAC == '1':
 	MEMLOC = 'unix:/usr/local/var/run/memcached/memcached.sock'
 	REDLOC1 = '/usr/local/var/run/redis/redis.sock'
 	REDLOC2 = '/usr/local/var/run/redis/redis2.sock' 
-	REDLOC3 = '/usr/local/var/run/redis/redis3.sock' 
+	REDLOC3 = '/usr/local/var/run/redis/redis3.sock'
+	REDLOC4 = '/usr/local/var/run/redis/redis4.sock' 
 else:
 	MEMLOC = 'unix:/var/run/memcached/memcached.sock'
 	REDLOC1 = '/var/run/redis/redis.sock'
 	REDLOC2 = '/var/run/redis/redis2.sock'
 	REDLOC3 = '/var/run/redis/redis3.sock'
+	REDLOC3 = '/var/run/redis/redis4.sock'

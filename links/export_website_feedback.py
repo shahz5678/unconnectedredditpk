8aede2b4d807c93e0fcf6f563070065c4860f952
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-05 16:33:05 +0500

    minor tweak

--- links/export_website_feedback.py
+++ links/export_website_feedback.py
@@ -9,7 +9,7 @@ def export_error_feedback(request,*args,**kwargs):
 
 def export_website_feedback(request,*args,**kwargs):
 	feedback = get_website_feedback()
-	# keys = feedback[0].keys()
+	# remove answer6, question6 when reverting back to 5 question format
 	keys = ['username', 'answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'answer6',\
 	'question1', 'question2', 'score', 'question3', 'question4', 'question5', 'question6',\
 	'device', 'time_of_feedback', 'date_joined', 'city','gender','age','mobile']

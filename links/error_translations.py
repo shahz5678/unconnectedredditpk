0520327b02dc720c5d036ff95488d0159c2f7173
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-10 01:14:41 +0500

    initial commit

---
+++ links/error_translations.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+FORM_SUBMISSION_ERRORS = {\
+################ CREATE USERNAME ERRORS ################
+"Is safed patti mein naam likh kar OK dabao:":"اس سفید پٹی میں نام لکھ کر نیچے بٹن دباؤ",\
+"Naam mein sirf english harf, number ya @ _ . + - ho sakta hai":"نام میں صرف انگریزی حروف، نمبر اور @ _ + - کے نشان ہو سکتے ہیں۔ دوبارا کوشش کرو",\
+################ CREATE PASSWORD ERRORS ################
+"Safed patti mein password likho:":"سفید پٹی میں پاس ورڈ لکھو","Kam se kam 6 harf likhna zaruri hai!":"کم سے کم چھ حرف لکھنا ضروری ہے",\
+################ CREATE ACCOUNT ERRORS ################
+"password khali na choro":"","ye naam sahi nahi hai":"پاس ورڈ خالی نہ چھوڑو"}
---
+++ links/templatetags/get_ur.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from django.template.defaulttags import register
+from links.error_translations import FORM_SUBMISSION_ERRORS
+
+@register.filter(name='get_ur')
+def get_ur(string):
+	if 'kisi aur ka naam hai' in string:
+		return 'یہ نام پہل سے کسی نے رکھا ہوا ہے، کوئ اور نام رکھو'
+	elif 'nahi ho sakta!' in string:
+		return 'نام میں برے الفاظ نہ ڈالو'
+	elif 'ko boojhna aasan hai' in string:
+		return 'اس پاس ورڈ کا اندازہ لگانا آسان ہے۔ کچھ اور لکھو'
+	elif 'kiyunke naam mein hai' in string:
+		return 'پاس ورڈ میں نام نہ ڈالو'
+	elif 'naam tum se pehle' in string:
+		return 'یہ نام ابھی ابھی کسی اور نے رکھ لیا۔ معذرت'
+	else:
+		return FORM_SUBMISSION_ERRORS[string]

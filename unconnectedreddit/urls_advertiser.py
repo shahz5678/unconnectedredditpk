9fb2b866b1cb95b5f36b373accf9fd36a37ee3a6
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-09 05:48:43 +0500

    imposing auth() reqs on advertisers

--- unconnectedreddit/urls_advertiser.py
+++ unconnectedreddit/urls_advertiser.py
@@ -1,12 +1,11 @@
 from links.views import advertise_with_us, show_advertisers
 from django.contrib.auth.decorators import login_required as auth
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^show_advertisers/', auth(show_advertisers), name='show_advertisers'),
-	url(r'^advertise_with_us/', advertise_with_us, name='advertise_with_us'),
+	url(r'^advertise_with_us/', auth(advertise_with_us), name='advertise_with_us'),
 )

a50ba1dce129ef1a6dea4352d3a0978eeb273849
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-13 03:35:32 +0500

    added Opti function

--- links/webhooks.py
+++ links/webhooks.py
@@ -7,24 +7,24 @@ from django.http import HttpResponse
 # from optimizely_config_manager import OptimizelyConfigManager
 # config_manager = OptimizelyConfigManager(PID)
 
-# @csrf_exempt
-# def webhook_event(request,*args,**kwargs):
-# 	if request.method == 'POST':
-# 		data = json.loads(request.body)
-# 		event_type = data['event']
-# 		# use CDN URL from webhook payload
-# 		if event_type == 'project.datafile_updated':
-# 			url = data['data']['cdn_url']
-# 			config_manager.set_obj(url)
-# 			response_data = {}
-# 			response_data['success'] = True
-# 			# return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-# 		response_data = {}
-# 		response_data['success'] = False
-# 		# return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
-# 		return HttpResponse(json.dumps(response_data), content_type="application/json")
-# 	else:
-# 		return render(request,"404.html",{})
+@csrf_exempt
+def webhook_event(request,*args,**kwargs):
+	if request.method == 'POST':
+		data = json.loads(request.body)
+		event_type = data['event']
+		# use CDN URL from webhook payload
+		if event_type == 'project.datafile_updated':
+			url = data['data']['cdn_url']
+			config_manager.set_obj(url)
+			response_data = {}
+			response_data['success'] = True
+			# return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+		response_data = {}
+		response_data['success'] = False
+		# return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+		return HttpResponse(json.dumps(response_data), content_type="application/json")
+	else:
+		return render(request,"404.html",{})
 # #######################
 
 @csrf_exempt

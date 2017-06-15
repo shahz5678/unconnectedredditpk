8b035170ceec73ef7407a65092e7034179a6fe84
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-15 12:44:40 +0500

    added virgin tees url pattern

--- unconnectedreddit/urls_ads.py
+++ unconnectedreddit/urls_ads.py
@@ -1,4 +1,4 @@
-from links.views import skin_clinic, skin_doctor_price, ad_feedback
+from links.views import skin_clinic, skin_doctor_price, ad_feedback, virgin_tees
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	#############################################Ads##########################################
 	url(r'^ad_feedback/', auth(ad_feedback), name='ad_feedback'),
+	url(r'^vtees/', auth(virgin_tees), name='virgin_tees'),
 	url(r'^skin_clinic/', auth(skin_clinic), name='skin_clinic'),
 	url(r'^skin_doctor_price/', auth(skin_doctor_price), name='skin_doctor_price'),
 )

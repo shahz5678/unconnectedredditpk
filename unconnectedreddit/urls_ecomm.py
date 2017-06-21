898fc0f8966c59e80d23b4cb9183259756e62fba
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-21 14:35:16 +0500

    added classifieds functionality

--- unconnectedreddit/urls_ecomm.py
+++ unconnectedreddit/urls_ecomm.py
@@ -1,4 +1,6 @@
-from links.ecomm import show_ecomm_choices, x2lite_details, x32_details, buyer_loc, process_city
+from links.ecomm import add_shop, shops_by_city, show_shop_choices, x2lite_details, x32_details, buyer_loc, process_city, post_basic_item,\
+post_seller_info, post_basic_item_photos, init_classified #, ecomm_welcome
+from links.number_verification import verify_basic_item_seller_number
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
@@ -7,9 +9,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	#############################################E Commerce##########################################
-	url(r'^mobile/$', show_ecomm_choices,name='ecomm'),
-	url(r'^mobile/x32/$', x32_details,name='x32'),
-	url(r'^mobile/x2lite/$', x2lite_details,name='x2lite'),
-	url(r'^mobile/loc/$', auth(buyer_loc),name='buyer_loc'),
-	url(r'^mobile/proc_city/$', auth(process_city),name='process_city'),
+	url(r'^item_ki_detail_likho/$', auth(post_basic_item),name='post_basic_item'),
+	url(r'^baichney_waley_ki_detail_likho/$', auth(post_seller_info),name='post_seller_info'),
+	url(r'^item_ki_pics_lagao/$', auth(post_basic_item_photos),name='post_basic_item_photos'),
+	url(r'^kuch_baicho/$', auth(init_classified),name='init_classified'),
+	# url(r'^baichnay_ka_tareeqa/$', auth(),name=''),
+	url(r'^vsn/$',verify_basic_item_seller_number, name="verify_basic_item_seller_number"),
+	############################################################
+	url(r'^shop_city/(?P<city>[\w.@+-]+)/$', auth(shops_by_city),name='shops_by_city'),
+	url(r'^add_shop/$', auth(add_shop),name='add_shop'),
+	url(r'^mobile/$', auth(show_shop_choices),name='ecomm'),
+	# url(r'^mobile/x32/$', x32_details,name='x32'),
+	# url(r'^mobile/x2lite/$', x2lite_details,name='x2lite'),
+	# url(r'^mobile/loc/$', auth(buyer_loc),name='buyer_loc'),
+	# url(r'^mobile/proc_city/$', auth(process_city),name='process_city'),
 )

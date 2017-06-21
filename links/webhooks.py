39f36a88ef6afc73e42c14ef335a45cc0d7dc615
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-21 15:07:39 +0500

    reverting

--- links/webhooks.py
+++ links/webhooks.py
@@ -1,36 +1,27 @@
 import json
+from optimizely_config_manager import OptimizelyConfigManager
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-# #######################
-# from unconnectedreddit.optimizely_settings import PID
-# from optimizely_config_manager import OptimizelyConfigManager
-# config_manager = OptimizelyConfigManager(PID)
+from unconnectedreddit.optimizely_settings import PID
 
-# @csrf_exempt
-# def webhook_event(request,*args,**kwargs):
-# 	if request.method == 'POST':
-# 		data = json.loads(request.body)
-# 		event_type = data['event']
-# 		# use CDN URL from webhook payload
-# 		if event_type == 'project.datafile_updated':
-# 			url = data['data']['cdn_url']
-# 			config_manager.set_obj(url)
-# 			response_data = {}
-# 			response_data['success'] = True
-# 			# return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-# 		response_data = {}
-# 		response_data['success'] = False
-# 		# return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
-# 		return HttpResponse(json.dumps(response_data), content_type="application/json")
-# 	else:
-# 		return render(request,"404.html",{})
-# #######################
+config_manager = OptimizelyConfigManager(PID)
 
 @csrf_exempt
 def webhook_event(request,*args,**kwargs):
 	if request.method == 'POST':
 		data = json.loads(request.body)
-		return render(request,"account_kit_test.html",{'data':data})
+		event_type = data['event']
+		# use CDN URL from webhook payload
+		if event_type == 'project.datafile_updated':
+			url = data['data']['cdn_url']
+			config_manager.set_obj(url)
+			response_data = {}
+			response_data['success'] = True
+			# return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+		response_data = {}
+		response_data['success'] = False
+		# return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+		return HttpResponse(json.dumps(response_data), content_type="application/json")
 	else:
 		return render(request,"404.html",{})

0a8a471b1eca9c3e0fd041a83ed99ac872ae2267
mhb11 <hassan.baig@alumni.duke.edu>
2017-06-24 09:37:14 +0500

    minor tweaks

--- links/views.py
+++ links/views.py
@@ -488,9 +488,9 @@ def star_list(request, *args, **kwargs):
 	ids = UserFan.objects.filter(fan_id=pk).values_list('star_id',flat=True).order_by('-fanning_time')
 	page_num = request.GET.get('page', '1')
 	page_obj = get_page_obj(page_num,ids,STARS_PER_PAGE)
-	users1 = {x['id']:x for x in User.objects.select_related('userprofile').filter(id__in=page_obj.object_list).\
+	users = {x['id']:x for x in User.objects.filter(id__in=page_obj.object_list).\
 	values('id','username','userprofile__score','userprofile__avatar').annotate(photo_count=Count('photo', distinct=True))}
-	users_with_photo_counts = [users1[id] for id in page_obj.object_list]
+	users_with_photo_counts = [users[id] for id in page_obj.object_list]
 	context["page_obj"] = page_obj
 	users_with_photo_thumbs = retrieve_thumbs(users_with_photo_counts)
 	context["users"] = users_with_photo_thumbs

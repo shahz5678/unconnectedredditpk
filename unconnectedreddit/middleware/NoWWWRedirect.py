101719b0c50c5f9fd60dd50c29a0c8672e992980
mhb11 <hassan.baig@alumni.duke.edu>
2017-07-26 03:25:47 +0500

    middleware that redirects WWW to NoWWW

---
+++ unconnectedreddit/middleware/NoWWWRedirect.py
@@ -0,0 +1,10 @@
+from django import http
+
+class NoWWWRedirectMiddleware(object):
+    def process_request(self, request):
+        host = request.get_host()
+        if host.startswith('www.'):
+            if request.method == 'GET':  # if wanna be a prefect REST citizen, consider HEAD and OPTIONS here as well
+                no_www_host = host[4:]
+                url = request.build_absolute_uri().replace(host, no_www_host, 1)
+                return http.HttpResponsePermanentRedirect(url)

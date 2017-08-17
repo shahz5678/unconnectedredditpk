e0a406785561dcbfb0008a7798260b3cf2a272c0
mhb11 <hassan.baig@alumni.duke.edu>
2017-08-17 23:58:20 +0500

    added verify_user_number

--- links/number_verification.py
+++ links/number_verification.py
@@ -1,9 +1,10 @@
+from score import NUMBER_VERIFICATION_BONUS
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse_lazy
 from account_kit_config_manager import account_kit_handshake
 from redis4 import save_careem_data, save_number_verification_error_data
-from tasks import save_consumer_credentials, set_user_binding_with_twilio_notify_service
-from redis3 import save_basic_ad_data, someone_elses_number, get_temporarily_saved_ad_data, get_buyer_snapshot#, reset_temporarily_saved_ad
+from tasks import save_consumer_credentials, set_user_binding_with_twilio_notify_service, increase_user_points
+from redis3 import save_basic_ad_data, someone_elses_number, get_temporarily_saved_ad_data, get_buyer_snapshot, get_user_csrf, is_mobile_verified
 
 def get_requirements(request, csrf, careem=False):
 	status = request.GET.get('status', None)
@@ -41,6 +42,28 @@ def verify_careem_applicant(request,*args,**kwargs):
 		return render(request,"404.html",{})
 
 
+def verify_user_number(request,*args,**kwargs):
+	user_id = request.user.id
+	if is_mobile_verified(user_id):
+		return render(request,"already_verified.html",{})
+	else:
+		csrf = get_user_csrf(user_id=str(user_id))
+		if csrf:
+			AK_ID, MN_data, err = get_requirements(request=request,csrf=csrf)
+			if AK_ID and MN_data:
+				if someone_elses_number(national_number=MN_data['national_number'], user_id=user_id):
+					return render(request,"wrong_number.html",{'referrer':'home'})
+				else:
+					save_consumer_credentials.delay(AK_ID, MN_data, user_id)
+					increase_user_points.delay(user_id=user_id, increment=NUMBER_VERIFICATION_BONUS)
+					return render(request,"reward_earned.html",{})
+			elif err['status'] == "NOT_AUTHENTICATED":
+				return render(request,"dont_worry_just_authenticate.html",{'csrf':csrf,'referrer':'home','type':'user'})
+			else:
+				return render(request,"unverified_number.html",{'referrer':'home'})
+		else:
+			return render(request,"try_again.html",{'type':'user'})
+
 
 def verify_consumer_number(request,*args,**kwargs):
 	user_id = request.user.id
@@ -48,7 +71,7 @@ def verify_consumer_number(request,*args,**kwargs):
 	if data:
 		AK_ID, MN_data, err = get_requirements(request=request,csrf=data["csrf"])
 		if AK_ID and MN_data:
-			if someone_elses_number(MN_data['national_number'], user_id):
+			if someone_elses_number(national_number=MN_data['national_number'], user_id=user_id):
 				return render(request,"wrong_number.html",{'referrer':data["referrer"]})
 			else:
 				save_consumer_credentials.delay(AK_ID, MN_data, user_id)
@@ -70,7 +93,7 @@ def verify_basic_item_seller_number(request,*args,**kwargs):
 	CSRF = get_temporarily_saved_ad_data(user_id=str(user_id),only_csrf=True)
 	AK_ID, MN_data, err = get_requirements(request=request, csrf=CSRF)
 	if AK_ID and MN_data:
-		if someone_elses_number(MN_data["national_number"],user_id):
+		if someone_elses_number(national_number=MN_data["national_number"],user_id=user_id):
 			return render(request,"wrong_number.html",{'referrer':reverse_lazy("show_user_ads")})
 		else:
 			data = get_temporarily_saved_ad_data(user_id=str(user_id),full_ad=True)
@@ -81,7 +104,6 @@ def verify_basic_item_seller_number(request,*args,**kwargs):
 			set_user_binding_with_twilio_notify_service.delay(user_id=user_id, phone_number=MN_data["number"])
 			saved = save_basic_ad_data(context)
 		if saved:
-			# reset_temporarily_saved_ad(str(user_id)) #save_basic_ad_data already deletes temporarily_saved_ad
 			return render(request,"basic_item_ad_submitted.html",{})
 		else:
 			pass

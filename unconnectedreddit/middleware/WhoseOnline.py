5305d221c49a1dade7560db531769cd647e43b85
mhb11 <hassan.baig@alumni.duke.edu>
2017-08-29 02:11:46 +0500

    minor tweak

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -4,7 +4,7 @@ from links.redis4 import set_online_users
 class WhoseOnlineMiddleware(object):
 
     def process_request(self, request):
-    	if request.user.is_authenticated() and random.random() < 0.15:
+    	if random.random() < 0.15 and request.user.is_authenticated():
     		set_online_users(request.session['_auth_user_id'],request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
     	else:
     		pass

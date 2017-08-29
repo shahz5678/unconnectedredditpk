4a82ffecbed13a8d27d9549cf283f0d519906d47
mhb11 <hassan.baig@alumni.duke.edu>
2017-08-29 08:05:08 +0500

    minor optimization

--- links/redis2.py
+++ links/redis2.py
@@ -435,8 +435,7 @@ def public_group_vote_incr(group_id,priority):
 #saves the user's latest presence, to be used to show green, orange or grey blob
 def save_user_presence(user_id,group_id,epochtime):
 	my_server = redis.Redis(connection_pool=POOL)
-	user_presence = "up:"+str(user_id)+":"+str(group_id)
-	my_server.setex(user_presence,epochtime,100)
+	my_server.setex("up:"+str(user_id)+":"+str(group_id),epochtime,100)
 
 #gets the latest presence for all users participating in the most recent 25 replies
 def get_latest_presence(group_id, user_id_list):

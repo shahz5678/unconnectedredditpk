ef1823c2f0f5bfb08dad55fa6d15bdc33f9835ec
mhb11 <hassan.baig@alumni.duke.edu>
2017-08-29 07:33:12 +0500

    reverted code

--- links/forms.py
+++ links/forms.py
@@ -343,12 +343,12 @@ class PublicreplyForm(forms.ModelForm):
         return description
 
 class PublicreplyMiniForm(PublicreplyForm):
-    description = forms.CharField(max_length=250)
+    description = forms.CharField(max_length=250, error_messages={'required': 'Pehlay yahan jawab likho, phir "Jawab do" button dabao'})
 
     def __init__(self,*args,**kwargs):
         super(PublicreplyMiniForm, self).__init__(*args,**kwargs)
         self.fields['description'].widget.attrs['style'] = \
-        'background-color:#F8F8F8; width:98%; border: 1px solid #1f8cad;border-radius:5px;padding: 6px 6px 6px 0;text-indent: 6px;color: #1f8cad;'
+        'background-color:#F8F8F8;width:1000px;max-width:95%;border: 1px solid #1f8cad;border-radius:5px;padding: 6px 6px 6px 0;text-indent: 6px;color: #1f8cad;'
         self.fields['description'].widget.attrs['autocomplete'] = 'off'
 
 class SearchNicknameForm(forms.Form):

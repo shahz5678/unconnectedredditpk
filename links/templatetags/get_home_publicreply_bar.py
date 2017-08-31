10fea3b1b2b58b680785362cbb6d4d352df4ee35
mhb11 <hassan.baig@alumni.duke.edu>
2017-08-31 18:54:19 +0500

    removed a parameter

--- links/templatetags/get_home_publicreply_bar.py
+++ links/templatetags/get_home_publicreply_bar.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='home_publicreply_bar.html')
-def home_publicreply_bar(link_pk, lang, user_id, replyform, variation=None):
-	return {'link_id':link_pk,'lang':lang,'user_id':user_id,'replyform':replyform, 'variation':variation}
+def home_publicreply_bar(link_pk, lang, user_id, replyform):
+	return {'link_id':link_pk,'lang':lang,'user_id':user_id,'replyform':replyform}

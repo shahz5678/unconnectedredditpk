30f73551607826a0fd4d4c6516650b708169cb58
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-01 06:22:52 +0500

    tweaked

--- links/templatetags/css.py
+++ links/templatetags/css.py
@@ -3,13 +3,14 @@ from django import template
 register = template.Library()
 
 def htmlattributes(value, arg):
-    attrs = value.field.widget.attrs
-    data = arg.replace(' ', '')   
-    kvs = data.split(',')
-    for string in kvs:
-        kv = string.split(':')
-        attrs[kv[0]] = kv[1]
-    rendered = str(value)
-    return rendered
+	return value.as_widget(attrs={"style":arg})
+#     attrs = value.field.widget.attrs
+#     data = arg.replace(' ', '')   
+#     kvs = data.split(',')
+#     for string in kvs:
+#         kv = string.split(':')
+#         attrs[kv[0]] = kv[1]
+#     rendered = str(value)
+#     return rendered
 
 register.filter('htmlattributes', htmlattributes)

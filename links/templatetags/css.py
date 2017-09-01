0f60fbda669e86133c2500c612e305ea09604e21
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-01 09:59:53 +0500

    removed error logger

--- links/templatetags/css.py
+++ links/templatetags/css.py
@@ -1,12 +1,8 @@
 from django import template
-from links.redis4 import log_html_error
 
 register = template.Library()
 
 def htmlattributes(value, arg):
-	try:
-		return value.as_widget(attrs={"style":arg})
-	except:
-		log_html_error(value=value,arg=arg)
+	return value.as_widget(attrs={"style":arg})
 
 register.filter('htmlattributes', htmlattributes)

fa931a37e542df9c5972a4c05296faaae076834c
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-04 18:31:27 +0500

    Adding direct commenting functionality on photos

---
+++ links/templatetags/get_photo_comment_bar.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag(file_name='photo_comment_bar.html')
+def photo_comment_bar(photo_id, lang, sort_by, user_id, comment_form, origin):
+	return {'photo_id':photo_id,'lang':lang,'user_id':user_id,'comment_form':comment_form,'sort_by':sort_by,'origin':origin}

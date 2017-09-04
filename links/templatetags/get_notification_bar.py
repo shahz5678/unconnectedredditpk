241ea6f0ffdd8801280a23a045d777e38b3c70a8
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-04 08:55:41 +0500

    tweaked to accomodate best_photos

--- links/templatetags/get_notification_bar.py
+++ links/templatetags/get_notification_bar.py
@@ -6,10 +6,11 @@ from django.contrib.auth.models import User
 register = template.Library()
 
 @register.inclusion_tag(file_name='notification_bar.html')
-def notification_bar(notification, user, user_id, females, static_url, random, newest_user, lang=None, sort_by=None):
+def notification_bar(notification, origin, user, user_id, females, static_url, random, newest_user, lang=None, sort_by=None, is_home=None):
 	context = {'notification':notification}
 	if notification:
-		context ={'ident':user_id,'lang':lang,'sort_by':sort_by,'checked':females,'static_url':static_url, 'random':random, 'newest_user':newest_user}
+		context ={'ident':user_id,'lang':lang,'sort_by':sort_by,'checked':females,'static_url':static_url, 'random':random, 'newest_user':newest_user,\
+		'is_home':is_home, 'origin':origin}
 		object_type, freshest_reply, is_link, is_photo, is_groupreply, is_salat = GetLatest(user)
 		if not is_link and not is_photo and not is_groupreply and not is_salat:
 			context["latest_reply"] = []

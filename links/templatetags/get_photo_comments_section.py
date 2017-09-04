1eb633f2fef55920bc1c4a436bb9efd8dca35fe6
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-05 01:35:04 +0500

    extended functionality

--- links/templatetags/get_photo_comments_section.py
+++ links/templatetags/get_photo_comments_section.py
@@ -1,20 +1,25 @@
 from django import template
+from links.redis1 import get_raw_comments
 
 register = template.Library()
 
 @register.inclusion_tag(file_name='photo_comments_section.html')
-def photo_comments_section(raw_comments, static_url):
-	comments = []
-	raw_comment_set = filter(None,raw_comments.split('#el#'))
-	counter = 0
-	for raw_comment in reversed(raw_comment_set):
-		components = raw_comment.split("#",3)
-		comment = {}
-		comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["text"] = \
-		components[0], components[1], float(components[2]), components[3]
-		comments.append(comment)
-		counter += 1
-		if counter > 5:
-			# this ensure only 6 comments can show up on the photo page (at max)
-			break
-	return {'comments':comments, 'static_url':static_url}
+def photo_comments_section(raw_comments, static_url, origin=None):
+	# if originates from home ('3'), raw_comments contains photo_id in that case
+	if origin == '3':
+		raw_comments = get_raw_comments(raw_comments)
+	if raw_comments:
+		comments = []
+		raw_comment_set = filter(None,raw_comments.split('#el#'))
+		counter = 0
+		for raw_comment in reversed(raw_comment_set):
+			components = raw_comment.split("#",3)
+			comment = {}
+			comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["text"] = \
+			components[0], components[1], float(components[2]), components[3]
+			comments.append(comment)
+			counter += 1
+			if counter > 5:
+				# this ensure only 6 comments can show up on the photo page (at max)
+				break
+		return {'comments':comments, 'static_url':static_url}

a138aef1f210e29106001a7df3faa5144b288664
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-09 07:51:13 +0500

    added two more paramteres

--- links/templatetags/get_home_publicreply_bar.py
+++ links/templatetags/get_home_publicreply_bar.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='home_publicreply_bar.html')
-def home_publicreply_bar(link_pk, lang, sort_by, user_id, replyform):
-	return {'link_id':link_pk,'lang':lang,'user_id':user_id,'replyform':replyform,'sort_by':sort_by}
+def home_publicreply_bar(link_pk, link_writer_pk, lang, sort_by, user_id, replyform, origin):
+	return {'link_id':link_pk,'lang':lang,'user_id':user_id,'replyform':replyform,'sort_by':sort_by, 'link_writer_pk':link_writer_pk, 'origin':origin}

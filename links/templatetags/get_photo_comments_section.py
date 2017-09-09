50061b7686f721c24d30218ab4ae548ab56a9ad5
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-09 05:17:32 +0500

    added more parameters

--- links/templatetags/get_photo_comments_section.py
+++ links/templatetags/get_photo_comments_section.py
@@ -4,7 +4,7 @@ from links.redis1 import get_raw_comments
 register = template.Library()
 
 @register.inclusion_tag(file_name='photo_comments_section.html')
-def photo_comments_section(raw_comments, static_url, origin=None):
+def photo_comments_section(raw_comments, static_url, self_user_id, origin=None):
 	# if originates from home ('3'), raw_comments contains photo_id in that case
 	if origin == '3':
 		raw_comments = get_raw_comments(raw_comments)
@@ -13,13 +13,13 @@ def photo_comments_section(raw_comments, static_url, origin=None):
 		raw_comment_set = filter(None,raw_comments.split('#el#'))
 		counter = 0
 		for raw_comment in reversed(raw_comment_set):
-			components = raw_comment.split("#",3)
+			components = raw_comment.split("#",5)
 			comment = {}
-			comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["text"] = \
-			components[0], components[1], float(components[2]), components[3]
+			comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["commenter_id"], comment["photo_id"],comment["text"] = \
+			components[0], components[1], float(components[2]), components[3], components[4], components[5]
 			comments.append(comment)
 			counter += 1
 			if counter > 5:
 				# this ensure only 6 comments can show up on the photo page (at max)
 				break
-		return {'comments':comments, 'static_url':static_url}
+		return {'comments':comments, 'static_url':static_url,'self_user_id':self_user_id}

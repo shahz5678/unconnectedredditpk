a509b6b1e888b5404f1235688250d4abe53a4183
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-10 10:51:37 +0500

    added error export url

--- unconnectedreddit/urls_banning.py
+++ unconnectedreddit/urls_banning.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
 from links.judgement import enter_inter_user_ban, inter_user_ban_not_permitted, first_time_inter_user_banner, banned_users_list, change_ban_time, \
-user_ban_help, ban_underway, ban_leaderboard
+user_ban_help, ban_underway, ban_leaderboard, export_ban_error
 
 admin.autodiscover()
 
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
 	url(r'^user_ban_help/$', auth(user_ban_help), name='user_ban_help'),
 	url(r'^ftiub/$', auth(first_time_inter_user_banner), name='first_time_inter_user_banner'),
 	url(r'^ban_leaderboard/$', auth(ban_leaderboard), name='ban_leaderboard'),
+	url(r'^export_ban_error/$', auth(export_ban_error), name='export_ban_error'),
 )

69724f6374b1272f349feb1f25a3d7232a7e5b39
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-12 15:10:56 +0500

    initial commit

---
+++ links/templatetags/get_newbie_rules.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag(file_name='newbie_rules.html')
+def newbie_rules(location, static_url):
+	return {'location':location, 'static_url':static_url}

6ccc6884decc48ca58040f969f16343830f6dac0
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-13 18:52:06 +0500

    added a new SMS

--- links/send_sms.py
+++ links/send_sms.py
@@ -6,13 +6,15 @@ client = Client(ACCNT_SID, AT)
 service = client.notify.services(SERV_SID) # using twilio 6.4.3a 'alpha' library. This is experimental, and must be updated by September 2017 at least.
 
 
-def process_sms(target_number,ad_id,status=None, buyer_number=None):
+def process_sms(target_number,ad_id,status=None, buyer_number=None, item_name=None):
 	if status == 'unique_click_plus_expiry':
-		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Ab apka muft ad khatum ho gaya hai. 0311-5282297 per rabta karein new Ad ke charges ke liye".format(buyer_number)
+		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Ab apka ad khatam ho gaya hai. Damadam pe apna ad dubara laga lein".format(buyer_number)
 	elif status == 'unique_click':
 		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Doston ko apna ad dikhaney ke liye ye link dein: https://damadam.pk/ad/{1}/".format(buyer_number,ad_id)
 	elif status == 'approved':
-		body = "Damadam pe apka ad laga diya gaya hai. Doston ko ye link de ke ad dikhain: https://damadam.pk/ad/{0}/".format(ad_id)
+		body = "Apka ad Damadam pe lag gaya. Hum ne apko aik aur SMS bheji hai. Isko apney all friends ko forward kar dein. Us SMS mein apke ad ki tafseel hai"#Doston ko ye link de ke ad dikhain: https://damadam.pk/ad/{0}/".format(ad_id)
+	elif status == 'forward' and item_name:
+		body = "Mein {0} baich raha hoon. Aap ne ya apke dost ne kharidna hai? Tafseel yahan hai http://damadam.pk/ad/{1}/".format(item_name, ad_id)
 	message = client.messages.create(to=target_number, from_="+18328955063",body=body)
 	
 
@@ -34,10 +36,10 @@ def send_expiry_sms_in_bulk(user_ids):
                              body="Damadam pe apka muft ad khatam ho gaya hai. Neya ad laganey ke liye yahan ao: https://damadam.pk/kuch_baicho/")
 
 
-def get_all_bindings_to_date():
-	bindings = service.bindings .list(
-	        tag="sms",
-	        start_date=datetime.strptime("2017-01-30", "%Y-%m-%d")
-	    )
-	for binding in bindings:
-	    print(binding.sid)
+# def get_all_bindings_to_date():
+# 	bindings = service.bindings .list(
+# 	        tag="sms",
+# 	        start_date=datetime.strptime("2017-01-30", "%Y-%m-%d")
+# 	    )
+# 	for binding in bindings:
+# 	    print(binding.sid)

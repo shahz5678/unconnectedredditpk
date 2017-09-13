d17d2f26f187bff813106313ca929a6fa18a63d6
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-13 18:01:13 +0500

    minor tweak in SMS body

--- links/send_sms.py
+++ links/send_sms.py
@@ -10,7 +10,7 @@ def process_sms(target_number,ad_id,status=None, buyer_number=None):
 	if status == 'unique_click_plus_expiry':
 		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Ab apka muft ad khatum ho gaya hai. 0311-5282297 per rabta karein new Ad ke charges ke liye".format(buyer_number)
 	elif status == 'unique_click':
-		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Doston ko ye link de ke apna ad dikhain: https://damadam.pk/ad/{1}/".format(buyer_number,ad_id)
+		body = "Damadam pe apke ad se {0} ne apka mobile number lia. Doston ko apna ad dikhaney ke liye ye link dein: https://damadam.pk/ad/{1}/".format(buyer_number,ad_id)
 	elif status == 'approved':
 		body = "Damadam pe apka ad laga diya gaya hai. Doston ko ye link de ke ad dikhao: https://damadam.pk/ad/{0}/".format(ad_id)
 	message = client.messages.create(to=target_number, from_="+18328955063",body=body)

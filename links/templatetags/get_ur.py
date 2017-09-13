ca457b01f9cab7bbc23dc201f85d7fa1ea0de00f
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-13 23:40:36 +0500

    improved validation errors for login screen (urdu)

--- links/templatetags/get_ur.py
+++ links/templatetags/get_ur.py
@@ -19,10 +19,12 @@ def get_ur(string):
 	elif 'sirf english harf' in string:
 		return 'نام میں صرف انگریزی حروف یا نمبر ہو سکتے ہیں'
 	elif 'hamarey record mein nahi' in string:
-		return 'یہ نک نیم ہمارے رکارڈ میں نہیں ہے'
+		return 'یہ نک نیم ﮨمارے رکارڈ میں نہیں'
 	elif 'Agr bhool gaye ho' in string:
-		return 'پاس ورڈ غلط لکھا ہے ۔ اگر بھول گۓ ہو تو نیچے "پاس ورڈ بھول گۓ" کو دباؤ'
+		return 'پاس ورڈ غلط لکھا ہے ۔ اگر بھول گۓ ﮨو تو نیچے "پاس ورڈ بھول گۓ" کو دباؤ'
 	elif 'naam khali nah choro' in string:
 		return 'نک نیم خالی نہ چھوڑو'
+	elif 'chota harf bara'  in string:
+		return 'نک نیم میں یا بڑا حرف چھوٹا لکھ دیا ے، یا چھوٹا حرف بڑا'
 	else:
 		return FORM_SUBMISSION_ERRORS[string]

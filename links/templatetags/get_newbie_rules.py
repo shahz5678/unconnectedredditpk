c44169abf97ee68f4eeff6b81039d165dd3e3320
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-17 13:35:14 +0500

    added a parameter

--- links/templatetags/get_newbie_rules.py
+++ links/templatetags/get_newbie_rules.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='newbie_rules.html')
-def newbie_rules(location, static_url):
-	return {'location':location, 'static_url':static_url}
+def newbie_rules(location, static_url, lang=None):
+	return {'location':location, 'static_url':static_url, 'lang':lang}

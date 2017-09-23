aa2f091708716e78d15188919075e05a7637dce3
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-24 04:39:57 +0500

    maintenance operations

---
+++ unconnectedreddit/urls_maint.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required as auth
+from django.contrib import admin
+from links.maint_views import insert_nicks, change_nicks, export_nicks, deprecate_nicks
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+	url(r'^populate_nicks/$', auth(insert_nicks), name='insert_nicks'),
+	url(r'^change_nicks/$', auth(change_nicks), name='change_nicks'),
+	url(r'^export_nicks/$', auth(export_nicks), name='export_nicks'),
+	url(r'^deprecate_nicks/$', auth(deprecate_nicks), name='deprecate_nicks'),
+)

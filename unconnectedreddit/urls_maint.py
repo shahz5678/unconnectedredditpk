d3b57fc88e66ca3315deeae50ec9a56016f8a441
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-27 03:24:17 +0500

    added new url pattern

--- unconnectedreddit/urls_maint.py
+++ unconnectedreddit/urls_maint.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
 from links.maint_views import insert_nicks, change_nicks, export_nicks, deprecate_nicks, remove_inactives_notification_activity, \
-remove_inactive_user_sessions
+remove_inactive_user_sessions, remove_inactives_groups
 
 
 admin.autodiscover()
@@ -10,6 +10,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	######################################## Redis Sanitzation ########################################
+	url(r'^cleanup_groups/$', auth(remove_inactives_groups), name='remove_inactives_groups'),
 	url(r'^delete_sessions/$', auth(remove_inactive_user_sessions), name='remove_inactive_user_sessions'),
 	url(r'^delete_notifications/$', auth(remove_inactives_notification_activity), name='remove_inactives_notification_activity'),
 	######################################## Username Sanitzation ########################################

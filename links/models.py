2ff20aa7aef4c93d5d781ddae473df987f5b990e
mhb11 <hassan.baig@alumni.duke.edu>
2017-09-27 05:19:38 +0500

    added db_index=True

--- links/models.py
+++ links/models.py
@@ -135,7 +135,7 @@ class Link(models.Model):
 	url = models.URLField("website (agr hai):", max_length=250, blank=True)
 	cagtegory = models.CharField("Category", choices=CATEGS, default=1, max_length=25)
 	image_file = models.ImageField("Photo charhao:",upload_to=upload_to_location, storage=OverwriteStorage(), null=True, blank=True )
-	latest_reply = models.ForeignKey('links.Publicreply', blank=True, null=True, on_delete=models.CASCADE)#models.SET_NULL)
+	latest_reply = models.ForeignKey('links.Publicreply', blank=True, null=True, db_index=True, on_delete=models.CASCADE)#models.SET_NULL)
 	
 	with_votes = LinkVoteCountManager() #change this to set_rank()
 	objects = models.Manager() #default, in-built manager

ffd2cd0fd99b0e41c1db7cbe7c85cfc58c11243f
mhb11 <hassan.baig@alumni.duke.edu>
2017-10-02 20:00:29 +0500

    added new url pattern for photo deprecation

--- unconnectedreddit/urls_maint.py
+++ unconnectedreddit/urls_maint.py
@@ -2,16 +2,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
 from links.maint_views import insert_nicks, change_nicks, export_nicks, deprecate_nicks, remove_inactives_notification_activity, \
-remove_inactive_user_sessions, remove_inactives_groups, damadam_cleanup, skip_outage_notif
+remove_inactive_user_sessions, remove_inactives_groups, damadam_cleanup, skip_outage_notif, remove_inactives_photos
 
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-	##################################### Shutdown Notifications ######################################
+	###################################### Shutdown Notifications ########################################
 	url(r'^son/$', auth(skip_outage_notif), name='skip_outage_notif'),
 	url(r'^damadam_cleanup/$', auth(damadam_cleanup), name='damadam_cleanup'),
+	########################################## PG Sanitzation ############################################
+	url(r'^cleanup_photos/$', auth(remove_inactives_photos), name='remove_inactives_photos'),
 	######################################## Redis Sanitzation ###########################################
 	url(r'^cleanup_groups/$', auth(remove_inactives_groups), name='remove_inactives_groups'),
 	url(r'^delete_sessions/$', auth(remove_inactive_user_sessions), name='remove_inactive_user_sessions'),

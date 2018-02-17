e2ddc92273f4c7422a6c9bee029c9a4fccdd8119
mhb11 <hassan.baig@alumni.duke.edu>
2018-02-18 00:30:58 +0500

    changed copy

--- links/error_translations.py
+++ links/error_translations.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 FORM_SUBMISSION_ERRORS = {\
 ################ CREATE USERNAME ERRORS ################
-"Is safed patti mein naam likh kar OK dabao:":"اس سفید پٹی میں نام لکھ کر نیچے بٹن دباؤ",\
-"Naam mein sirf english harf, number ya @ _ . + - ho sakta hai":"نام میں صرف انگریزی حروف، نمبر اور @ _ + - کے نشان ہو سکتے ہیں۔ دوبارا کوشش کرو",\
+"Is safed patti mein nickname likh ke OK dabain:":"اس سفید پٹی میں نام لکھ کر نیچے بٹن دباؤ",\
+"Nickname mein sirf english harf, number ya @ _ . + - likhein":"نام میں صرف انگریزی حروف، نمبر اور @ _ + - کے نشان ہو سکتے ہیں۔ دوبارا کوشش کرو",\
 ################ CREATE PASSWORD ERRORS ################
-"Safed patti mein password likho:":"سفید پٹی میں پاس ورڈ لکھو","Kam se kam 6 harf likhna zaruri hai!":"کم سے کم چھ حرف لکھنا ضروری ہے",\
+"Safed patti mein password likhein:":"سفید پٹی میں پاس ورڈ لکھو",\
+"Kam se kam 6 harf likhna zaruri hain!":"کم سے کم چھ حرف لکھنا ضروری ہے",\
 ################ CREATE ACCOUNT ERRORS ################
-"password khali nah choro":"پاس ورڈ خالی نہ چھوڑو","ye naam sahi nahi hai":"پاس ورڈ خالی نہ چھوڑو"}
+"password khali nah chorein":"پاس ورڈ خالی نہ چھوڑو","ye nickname sahi nahi hai":"پاس ورڈ خالی نہ چھوڑو"}

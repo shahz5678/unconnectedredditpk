6252fff5ac95da0d3d7a43895ada83cd09529df7
mhb11 <hassan.baig@alumni.duke.edu>
2018-04-04 02:41:15 +0500

    added functionality to display default avatar when user hasn't uploaded an avatar

--- links/templatetags/s3.py
+++ links/templatetags/s3.py
@@ -1,22 +1,34 @@
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django import template
 import string
 
 register = template.Library()
 
 @register.filter(name='s3')
-def get_s3_object(filename,category='image'):
-	filename = str(filename)
-	if "photos/" in filename:
-		split_by = "photos/"
-	elif "avatars/" in filename:
-		split_by = "avatars/"
-	elif "mehfils/" in filename:
-		split_by = "mehfils/"
-	elif "users/" in filename:
-		split_by = "users/"
+def get_s3_object(filename,category='img'):
+	if not filename:
+		if category == 'thumb':
+			return static('img/default-avatar-min.jpg')
+		else:
+			return None
 	else:
-		split_by = "photos/"
-	if category=='thumb':
-		return "//s3.eu-central-1.amazonaws.com/damadam/thumbnails/"+filename.split(split_by)[1]
-	else:
-		return "//s3.eu-central-1.amazonaws.com/damadam/"+split_by+filename.split(split_by)[1]
+		filename = str(filename)
+		if "photos/" in filename:
+			split_by = "photos/"
+		elif "personal_groups/" in filename:
+			split_by = "personal_groups/"
+		elif "avatars/" in filename:
+			split_by = "avatars/"
+		elif "mehfils/" in filename:
+			split_by = "mehfils/"
+		elif "users/" in filename:
+			split_by = "users/"
+		else:
+			split_by = "photos/"
+		if category=='thumb':
+			return "//s3.eu-central-1.amazonaws.com/damadam/thumbnails/"+filename.split(split_by)[1]
+		else:
+			try:
+				return "//s3.eu-central-1.amazonaws.com/damadam/"+split_by+filename.split(split_by)[1]
+			except IndexError:
+				return None

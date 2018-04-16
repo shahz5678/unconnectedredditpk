6cef2227c88ad047b8b6b62c8a1ede49e92a2f25
mhb11 <hassan.baig@alumni.duke.edu>
2018-04-17 00:21:38 +0500

    reverted change

--- links/templatetags/truncap.py
+++ links/templatetags/truncap.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.filter(name='truncap')
 def truncate_and_capitalize(s):
-	return s.decode('utf-8')[:1].upper()
-	# return s[:1].upper()
+	# return s.decode('utf-8')[:1].upper()
+	return s[:1].upper()

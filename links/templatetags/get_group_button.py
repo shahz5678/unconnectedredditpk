7d40c1cc89b05014b4dc9a5fa4e1d6b6bea59c30
mhb11 <hassan.baig@alumni.duke.edu>
2018-04-16 09:35:52 +0500

    fixed a bug regarding unauthenticated users

--- links/templatetags/get_group_button.py
+++ links/templatetags/get_group_button.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.inclusion_tag(file_name='personal_group/buttons/group_button.html')
 def group_button(own_id, target_id, parent_object_id, object_type, origin, target_username=None):
-	if int(own_id) == int(target_id):
+	if not own_id or int(own_id) == int(target_id):
 		return {'own_id':True}
 	else:
 		return {'own_id':False, 'tid':target_id, 'poid':parent_object_id, 'ot':object_type, 'org':origin, 'tun':target_username}

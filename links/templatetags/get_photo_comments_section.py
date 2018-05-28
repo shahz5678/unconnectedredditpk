33ec7aca1f369d540c232ec8263848e54de49547
mhb11 <hassan.baig@alumni.duke.edu>
2018-05-28 19:07:44 +0500

    added a condition to perform exception handling

--- links/templatetags/get_photo_comments_section.py
+++ links/templatetags/get_photo_comments_section.py
@@ -13,11 +13,12 @@ def photo_comments_section(raw_comments, static_url, self_user_id, origin=None, 
 		raw_comment_set = filter(None,raw_comments.split('#el#'))
 		counter = 0
 		for raw_comment in reversed(raw_comment_set):
-			components = raw_comment.split("#",5)
-			comment = {}
-			comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["commenter_id"], comment["photo_id"],comment["text"] = \
-			components[0], components[1], float(components[2]), components[3], components[4], components[5]
-			comments.append(comment)
+			if raw_comment[0] != '#':# exception handling
+				components = raw_comment.split("#",5)
+				comment = {}
+				comment["writer_av_url"], comment["writer_uname"], comment["epoch_time"], comment["commenter_id"], comment["photo_id"],comment["text"] = \
+				components[0], components[1], float(components[2]), components[3], components[4], components[5]
+				comments.append(comment)
 			counter += 1
 			if counter > 5:
 				# this ensure only 6 comments can show up on the photo page (at max)

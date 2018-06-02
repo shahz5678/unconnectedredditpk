8540969773dac395baacbaa26821257474f8ee9a
mhb11 <hassan.baig@alumni.duke.edu>
2018-06-02 21:12:48 +0500

    logging profile views

--- links/tasks.py
+++ links/tasks.py
@@ -26,7 +26,7 @@ personal_group_hard_deletion, exited_personal_group_hard_deletion, update_person
 rate_limit_personal_group_sharing
 from redis4 import expire_online_users, get_recent_online, set_online_users, log_input_rate, log_input_text, retrieve_uname, retrieve_avurl, \
 retrieve_credentials, invalidate_avurl, increment_convo_counter, increment_session, track_p2p_sms, check_p2p_sms, log_personal_group_exit_or_delete,\
-log_share, logging_sharing_metrics, cache_photo_share_data#, log_photo_attention_from_fresh
+log_share, logging_sharing_metrics, cache_photo_share_data, logging_profile_view#, log_photo_attention_from_fresh
 from redis2 import set_benchmark, get_uploader_percentile, bulk_create_photo_notifications_for_fans, remove_erroneous_notif,\
 bulk_update_notifications, update_notification, create_notification, update_object, create_object, add_to_photo_owner_activity,\
 get_active_fans, public_group_attendance, clean_expired_notifications, get_top_100,get_fan_counts_in_bulk, get_all_fans, is_fan, \
@@ -125,6 +125,11 @@ def punish_gibberish_writers(dict_of_targets):
 	for user_id, score_penalty in dict_of_targets.items():
 		UserProfile.objects.filter(user_id=user_id).update(score=F('score')-score_penalty)
 		queue_punishment_amount(user_id,score_penalty)
+
+
+@celery_app1.task(name='tasks.cache_photo_shares')
+def log_profile_view(visitor_id,star_id,viewing_time):
+	logging_profile_view(visitor_id,star_id,viewing_time)
 
 
 @celery_app1.task(name='tasks.cache_photo_shares')
@@ -414,6 +419,7 @@ def calc_gibberish_punishment():
 			for k in all_gibberish_writers_above_threshold.keys():
 				all_gibberish_writers_above_threshold[k] *= GIBBERISH_PUNISHMENT_MULTIPLIER
 			punish_gibberish_writers(all_gibberish_writers_above_threshold)
+
 
 @celery_app1.task(name='tasks.calc_photo_quality_benchmark')
 def calc_photo_quality_benchmark():

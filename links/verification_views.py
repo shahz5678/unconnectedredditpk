baa4a499a966fcbc4de904db8b69448949834136
Fahad Rao <fahadrao@gmail.com>
2018-09-26 22:56:21 +0500

    homemade_mobile_verification v1

--- links/verification_views.py
+++ links/verification_views.py
@@ -3,10 +3,14 @@ from django.db.models import F
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.cache import cache_control
-from verification_forms import AddVerifiedUserForm, rate_limit_artificial_verification
-from redis3 import save_consumer_number
+from verification_forms import AddVerifiedUserForm, rate_limit_artificial_verification, MobileVerificationForm,PinVerifyForm
+from tasks import send_user_pin, save_consumer_credentials, increase_user_points
+from redis3 import save_consumer_number, rate_limit_sms_sending, retrieve_random_pin
 from redis4 import retrieve_uname
 from models import UserProfile
+from score import NUMBER_VERIFICATION_BONUS
+
+
 
 @cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 @csrf_protect
@@ -42,3 +46,65 @@ def verify_user_artificially(request):
 			return render(request,"verification/artificial_verification.html",{'form':AddVerifiedUserForm()})
 	else:
 		return redirect('missing_page')
+
+############################## User number verification #################################
+# @csrf_protect
+# def buyer_details(request,origin,*args,**kwargs):
+
+
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
+@csrf_protect
+def verify_user_mobile(request):
+	"""
+	This renders a template where user puts her number that she wants verified
+
+	"""
+	user_id = request.user.id
+	if request.method == "POST":
+		form = MobileVerificationForm(request.POST,user_id=user_id)
+		if form.is_valid():
+			phonenumber = form.cleaned_data.get("phonenumber")
+			user_id = request.user.id
+			pin = retrieve_random_pin(user_id)
+			target_number = '+92'+phonenumber[-10:]
+			send_user_pin.delay(target_number,pin)
+			rate_limit_sms_sending(user_id)
+			form = PinVerifyForm
+			request.session['phonenumber']=phonenumber	
+			request.session.modified = True
+			return render(request,"verification/enter_pin_code.html",{'form':form,'phonenumber':phonenumber})
+		else:
+			return render(request,'verification/user_mobile_verification.html',{'form':form})
+	else:		
+		form = MobileVerificationForm()
+		return render(request,'verification/user_mobile_verification.html',{'form':form})
+	
+
+
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
+@csrf_protect
+def pin_verification(request):
+	"""
+	This will verify the pin entered by the user
+
+	"""
+	if request.method == "POST":
+		user_id = request.user.id
+		form = PinVerifyForm(request.POST,user_id=user_id)
+		phonenumber = request.session.get('phonenumber',None)
+		if form.is_valid():
+			pin_state = form.cleaned_data.get("pinnumber")
+			if pin_state == 'pin_matched':
+				account_kid_id = 'twilio_verification'
+				national_number = phonenumber[-10:]
+				number ='+92'+national_number	
+				mobile_data = {'national_number':national_number,'number':number,'country_prefix':'92'}
+				save_consumer_credentials.delay(account_kid_id, mobile_data, user_id)
+				increase_user_points.delay(user_id=user_id, increment=NUMBER_VERIFICATION_BONUS)
+				return render(request,"reward_earned.html",{})
+		else:
+			return render(request,"verification/enter_pin_code.html",{'form':form,'phonenumber':phonenumber})
+	else:
+		return redirect('missing_page')
+
+	

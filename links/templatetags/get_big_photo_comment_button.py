03844f9e57fe8e6a19441bce193c1ed0593fa899
Fahad Rao <fahadrao@gmail.com>
2019-03-04 15:49:59 +0500

    Mobile optimized design update v1, nerfed world age for overperforming users

--- links/templatetags/get_big_photo_comment_button.py
+++ links/templatetags/get_big_photo_comment_button.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='big_buttons/big_photo_comment_button.html')
-def big_photo_comment_button(photo_id, origin, comm_count, static_url, star_id=None, seen=None):
+def big_photo_comment_button(photo_id, origin, comm_count, static_url, star_id=None, seen=None, from_single_notif=None):
 	comm_count = int(comm_count) if comm_count else 0
 	return {'photo_id':photo_id,'origin':origin,'comm_count':comm_count,'static_url':static_url,'star_id':star_id,\
-	'border_color':'#BFBFBF' if seen else '#ff8000'}
+	'border_color':'#BFBFBF' if seen else '#ff8000','from_single_notif':from_single_notif}

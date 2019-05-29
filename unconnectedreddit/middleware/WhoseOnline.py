57d1397b5a231ac39902934fbd24b6e9e1ee5e13
Fahad Rao <fahadrao@gmail.com>
2019-05-29 14:07:14 +0500

    bugfix in online list related to world age, threshold updated for online related middleware

--- unconnectedreddit/middleware/WhoseOnline.py
+++ unconnectedreddit/middleware/WhoseOnline.py
@@ -6,12 +6,12 @@ class WhoseOnlineMiddleware(object):
 	def process_request(self, request):
 		ran_num = random.random()
 		####################################################################################
-		if ran_num <= 0.05 and request.user.is_authenticated():
-			# called between 0% - 5%
+		if ran_num <= 0.06 and request.user.is_authenticated():
+			# called between 0% - 6%
 			# set "online" list of users
 			save_online_user.delay(user_id=request.user.id, user_ip=request.META.get('X-IORG-FBS-UIP',request.META.get('REMOTE_ADDR')))
 		####################################################################################
-		elif ran_num >= 0.95 and request.user.is_authenticated():
-			# called between 95% - 100%
+		elif ran_num >= 0.94 and request.user.is_authenticated():
+			# called between 94% - 100%
 			# set user world age
 			set_user_age.delay(user_id=request.user.id)

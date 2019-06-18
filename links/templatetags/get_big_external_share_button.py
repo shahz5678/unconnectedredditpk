e7edc788cfd7418b6f7493f45ce20ef4e88d74de
mhb11 <hassan.baig@alumni.duke.edu>
2019-06-18 23:29:01 +0500

    changed a parameter

--- links/templatetags/get_big_external_share_button.py
+++ links/templatetags/get_big_external_share_button.py
@@ -3,8 +3,9 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='big_buttons/big_external_share_button.html')
-def big_external_share_button(owner_username, is_mobile, photo_id, origin, static_url, owner_id=None, sharer_id=None, js_class='', sharing_type=None\
-	, seen=None):
-	border_color = '#2CB742' if is_mobile else '#944CB2'
-	return {'owner_username':owner_username,'photo_id':photo_id,'static_url':static_url,'is_mobile':is_mobile,'origin':origin,\
-	'owner_id':owner_id,'sharer_id':sharer_id,'js_class':js_class,'sharing_type':sharing_type,'border_color':'#BFBFBF' if seen else border_color}
+def big_external_share_button(owner_username, on_opera, photo_id, origin, static_url, owner_id=None, sharer_id=None, js_class='', \
+	sharing_type=None, seen=None):
+	border_color = '#2CB742' if on_opera else '#944CB2'
+	return {'owner_username':owner_username,'photo_id':photo_id,'static_url':static_url,'on_opera':on_opera,'origin':origin,\
+	'owner_id':owner_id,'sharer_id':sharer_id,'sharing_type':sharing_type,'border_color':'#BFBFBF' if seen else border_color,\
+	'js_class':js_class}

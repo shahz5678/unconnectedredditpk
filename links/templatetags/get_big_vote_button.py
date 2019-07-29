d4eb7a77f377c958d3b4ed7208388116564d9eb6
mhb11 <hassan.baig@alumni.duke.edu>
2019-07-29 12:07:07 +0500

    fixed a bug

--- links/templatetags/get_big_vote_button.py
+++ links/templatetags/get_big_vote_button.py
@@ -7,7 +7,7 @@ def big_vote_button(is_pht, obid, origin, ooid, user_voted_set, static_url, topi
 	"""
 	is_pht must be '1' or '0'
 	"""
-	if user_voted_set and obid in user_voted_set:
+	if user_voted_set and str(obid) in user_voted_set:
 		return {'static_url':static_url,'obid':obid,'from':origin,'is_pht':is_pht,'ooid':ooid,'tp':topic, 'liked':True}
 	else:
 		return {'static_url':static_url,'obid':obid,'from':origin,'is_pht':is_pht,'ooid':ooid,'tp':topic, 'liked':False}

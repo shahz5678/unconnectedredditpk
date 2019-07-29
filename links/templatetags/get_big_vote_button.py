a0c05b1f04c421755d4be7c113419d3fdc3186ae
mhb11 <hassan.baig@alumni.duke.edu>
2019-07-29 13:13:59 +0500

    tweaked

--- links/templatetags/get_big_vote_button.py
+++ links/templatetags/get_big_vote_button.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag(file_name='big_buttons/big_vote_button.html')
-def big_vote_button(is_pht, obid, origin, ooid, user_voted_set, static_url, topic=None):
+def big_vote_button(is_pht, obid, origin, ooid, user_voted, static_url, topic=None):
 	"""
+	Helps render a 'like' button, in either its default or 'already-liked' state
+
 	is_pht must be '1' or '0'
+	'user_voted' contains 'True' if user has liked an object, otherwise it's simply None
 	"""
-	if user_voted_set and str(obid) in user_voted_set:
-		return {'static_url':static_url,'obid':obid,'from':origin,'is_pht':is_pht,'ooid':ooid,'tp':topic, 'liked':True}
-	else:
-		return {'static_url':static_url,'obid':obid,'from':origin,'is_pht':is_pht,'ooid':ooid,'tp':topic, 'liked':False}
+	return {'static_url':static_url,'obid':obid,'from':origin,'is_pht':is_pht,'ooid':ooid,'tp':topic, 'liked':user_voted}

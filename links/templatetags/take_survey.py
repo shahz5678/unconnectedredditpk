1cf87239b14e62f0bf0bd61be4ecfa6bfe9d9393
mhb11 <hassan.baig@alumni.duke.edu>
2019-08-07 17:57:17 +0500

    short-circuiting survey-related redis lookup

--- links/templatetags/take_survey.py
+++ links/templatetags/take_survey.py
@@ -1,12 +1,14 @@
 from django import template
-from links.redis4 import show_survey
+# from links.redis4 import show_survey
 
 register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def has_taken_survey(context):
-	user_id = context['request'].user.id
-	if user_id:
-		return show_survey(user_id)
-	else:
-		return True#assume non-logged in users have taken survey
+	return True
+	##############################################
+	# user_id = context['request'].user.id
+	# if user_id:
+	# 	return show_survey(user_id)
+	# else:
+	# 	return True#assume non-logged in users have taken survey

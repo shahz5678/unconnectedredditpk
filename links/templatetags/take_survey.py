3ffc8735975766a9922b45d73fdb4628642c3eeb
mhb11 <hassan.baig@alumni.duke.edu>
2019-08-09 18:11:06 +0500

    initializing nps survey

--- links/templatetags/take_survey.py
+++ links/templatetags/take_survey.py
@@ -1,14 +1,13 @@
 from django import template
-# from links.redis4 import show_survey
+from links.redis4 import show_survey
 
 register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def has_taken_survey(context):
-	return True
-	##############################################
-	# user_id = context['request'].user.id
-	# if user_id:
-	# 	return show_survey(user_id)
-	# else:
-	# 	return True#assume non-logged in users have taken survey
+	user_id = context['request'].user.id
+	if user_id:
+		# only show to those who took it before
+		return show_survey(user_id)
+	else:
+		return True#assume non-logged in users have taken survey

418f69b7ae23a7d6fa8cf7eab27821b50d6eed1f
mhb11 <hassan.baig@alumni.duke.edu>
2019-09-01 19:56:45 +0500

    updating proj_zuck starting user_id

--- links/score.py
+++ links/score.py
@@ -285,7 +285,7 @@ ZODIAC = {'1':'None','3':'Aquarius','4':'Pisces','5':'Aries','6':'Taurus','7':'G
 ###############################################################################################################
 SEGMENT_STARTING_TIME = 1560425907	# starting time of user segment analysis
 SEGMENT_STARTING_USER_ID = 19005280# starting user ID of user segment analysis
-PROJ_ZUCK_STARTING_USER_ID = 1962148
+PROJ_ZUCK_STARTING_USER_ID = 1975000
 """
 KEY:
 

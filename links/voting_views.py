ff1e5cb238b6e2ab1dbba5dda5202ff0024e2405
mhb11 <hassan.baig@alumni.duke.edu>
2019-10-02 13:21:56 +0500

    tweaked to enable gold stars

--- links/voting_views.py
+++ links/voting_views.py
@@ -9,7 +9,6 @@ from django.shortcuts import redirect, render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, Http404
 from django.middleware import csrf
-from verified import FEMALES
 from models import Link, Photo
 from redis2 import bulk_is_fan
 from colors import COLOR_GRADIENTS
@@ -22,7 +21,7 @@ from tasks import vote_tasks, cache_voting_history, log_user_activity
 from views import secs_to_mins, get_indices, beautiful_date, retrieve_user_env, convert_to_epoch
 from redis7 import get_obj_owner, voted_for_single_photo, voted_for_link, can_vote_on_obj, get_voting_details,retrieve_voting_records,\
 in_defenders, get_votes, check_content_and_voting_ban, is_obj_trending, retrieve_handpicked_photos_count, retrieve_global_voting_records,\
-retrieve_users_voting_relationships, retrieve_detailed_voting_data, log_section_wise_voting_liquidity
+retrieve_users_voting_relationships, retrieve_detailed_voting_data, log_section_wise_voting_liquidity, is_image_star
 from score import SEGMENT_STARTING_USER_ID
 
 
@@ -369,7 +368,7 @@ def show_voting_summary(request,pk,orig,pht):
 		return redirect("home")
 	topic = request.session.pop("origin_topic",'')
 	defender, voter_id_names_status_and_votes = False, []
-	is_pinkstar = True if oun in FEMALES else False
+	is_star = is_image_star(user_id=ooid)
 	exists, net_votes, upvotes, downvotes, pink_votes = get_voting_details(pk,pht)
 	show_banning_prompt = False
 	if exists:
@@ -385,7 +384,7 @@ def show_voting_summary(request,pk,orig,pht):
 			voter_id_names_status_and_votes = get_usernames(voters_and_votes, ban_status=True) if defender else []
 			show_banning_prompt = True if request.session.pop('show_banning_prompt',None) == '1' else False
 	return render(request,"voting/voting_summary.html",{'obj':obj,'exists':exists,'nv':net_votes,'uv':upvotes,'dv':downvotes,'pv':pink_votes,\
-		'obj_id':pk,'pht':pht,'is_pinkstar':is_pinkstar,'defender':defender,'voter_id_names_status_and_votes':voter_id_names_status_and_votes,\
+		'obj_id':pk,'pht':pht,'is_star':is_star,'defender':defender,'voter_id_names_status_and_votes':voter_id_names_status_and_votes,\
 		'tp':tp,'first_time_voting_judger':tutorial_unseen(user_id=own_id, which_tut='12', renew_lease=True) if exists else False, 'ooid':ooid,\
 		'oun':oun,'purl':purl,'lid':lid, 'show_banning_prompt':show_banning_prompt,'orig':orig,'own_id':str(own_id),'is_trending':trending_status,\
 		'time_of_trending':time_of_selection,'origin_topic':topic})
@@ -441,18 +440,14 @@ def export_voting_reputation_records(request):
 	own_id = request.user.id
 	is_defender, is_super_defender = in_defenders(own_id, return_super_status=True)
 	if is_super_defender:
-		data_to_write_to_csv = None#retrieve_voting_reputation_records()
+		data_to_write_to_csv = None
 		if data_to_write_to_csv:
 			import csv
-			filename = 'voting_reputation_data.csv'
+			filename = 'voting_and_content_reputation_data.csv'
 			with open(filename,'wb') as f:
 				wtr = csv.writer(f)
-				columns = ["voter ID", "world age disc","vote value", "voting time", "sybil status", "num clients", "target obj ID", \
-				"obj owner ID", "total upvotes (till this vote)", "was_handpicked", "time of selection"]
-				# 'sybil status' can be:
-				# '0': non-partisan ID
-				# '1': general sybil ID
-				# '2': direct sybil ID
+				columns = []
+				
 				wtr.writerow(columns)
 				for data in data_to_write_to_csv:
 					to_write = [data['vid'], data['wad'], 'upvote', data['vt'], data['ss'], data.get('num_sybs','-'),\

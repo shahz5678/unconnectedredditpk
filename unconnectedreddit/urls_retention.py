bc493d1c542836ec0f7353c00a0d9e90761116ee
mhb11 <hassan.baig@alumni.duke.edu>
2019-12-06 15:33:45 +0500

    added a test url'

--- unconnectedreddit/urls_retention.py
+++ unconnectedreddit/urls_retention.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required as auth
 from django.contrib import admin
 from links.retention_views import display_retention, export_retention_activity_raw_log, export_retention_activity_occurrence_log,\
-export_retention_activity_frequency_log, export_logged_actions#, reset_retention_counters
+export_retention_activity_frequency_log, export_logged_actions, test_sample_out#, reset_retention_counters
 
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 	url(r'^retention/export/raw/$', auth(export_retention_activity_raw_log), name='export_retention_activity_raw_log'),
 	url(r'^retention/export/freq/$', auth(export_retention_activity_frequency_log), name='export_retention_activity_frequency_log'),
 	url(r'^retention/export/occur/$', auth(export_retention_activity_occurrence_log), name='export_retention_activity_occurrence_log'),
+	url(r'^test_sample_out/(?P<uname>[\w.@+-]+)/(?P<ot>[\w.@+-]+)/(?P<origin>[\w.@+-]+)/(?P<home_hash>[\w.@+-]+)/(?P<poid>[\w.@+-]+)/(?P<tp>[\w.@+-]+)/(?P<tid>[\w.@+-]+)/$', auth(test_sample_out), name='test_sample_out'),
 	#################################################################################################
 	url(r'^export/actions/$', auth(export_logged_actions), name='export_logged_actions'),
 )

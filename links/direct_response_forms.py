fa66d98c0508742c17e4db363b79dffa72f0ec42
mhb11 <hassan.baig@alumni.duke.edu>
2019-12-10 02:10:19 +0500

    tweaked

--- links/direct_response_forms.py
+++ links/direct_response_forms.py
@@ -24,7 +24,7 @@ def prescribe_direct_response_rate_limit(typing_speed_1, text_1_len, typing_spee
 				# rate-limit this person
 				is_rate_limited = True
 		##################################
-		elif avg_typing_speed > 5.5:
+		elif typing_speed_1 > 5 and avg_typing_speed > 5.5:
 			# rate-limit this person
 			is_rate_limited = True
 
@@ -38,7 +38,7 @@ def prescribe_direct_response_rate_limit(typing_speed_1, text_1_len, typing_spee
 				# rate-limit this person
 				is_rate_limited = True
 		##################################
-		elif avg_typing_speed > 7.5:
+		elif typing_speed_1 > 5 and avg_typing_speed > 6.5:
 			# rate-limit this person
 			is_rate_limited = True
 

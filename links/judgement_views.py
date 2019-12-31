0e4329d685267bb992b863aeb811e673cb69a4e5
mhb11 <hassan.baig@alumni.duke.edu>
2019-12-31 16:26:14 +0500

    small tweak

--- links/judgement_views.py
+++ links/judgement_views.py
@@ -35,8 +35,7 @@ edit_content_and_voting_ban, is_ban_editing_locked, set_complaint, are_ids_unban
 from redis6 import retrieve_group_reqd_data, freeze_reported_group_functionality, retrieve_group_owner_id, retrieve_group_creation_time, \
 retrieve_group_rules, get_reported_group_info
 from redis4 import return_referrer_logs, retrieve_uname, retrieve_bulk_unames, freeze_critical_profile_functionality, retrieve_credentials
-from verified import FEMALES
-from redis9 import invalidate_cached_user_feed_history
+from redis2 import invalidate_cached_user_feed_history
 
 SEVEN_MINS = 7*60
 TWENTY_MINS = 20*60

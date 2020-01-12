386bc73b09f05637bad1de843cde86743fdfb5af
mhb11 <hassan.baig@alumni.duke.edu>
2020-01-13 02:09:08 +0500

    added support for showing 'new' labels on unseen content

--- links/templatetags/get_photo_object.py
+++ links/templatetags/get_photo_object.py
@@ -6,10 +6,10 @@ register = template.Library()
 @register.inclusion_tag(file_name='content/photo_object.html')
 def photo_object(pid, ooid, img_url, vsc, img_thumb ,upload_time, machine_time, oun, origin, static_url, caption, raw_caption, is_fbs=None, \
 	av_url=None, home_hash='',block_time_remaining=None,is_star=None, show_uname_credentials=True, show_report=True, show_trending=False,\
-	exclude_detail_url=False, lazyload_idx=1, ratio=None, nht=None, ot=None, aud=None, exp=None):
+	exclude_detail_url=False, lazyload_idx=1, ratio=None, nht=None, ot=None, aud=None, exp=None, is_new=None):
 	return {'vsc':float(vsc) if vsc else 0,'oun':oun, 'static_url':static_url, 'VDC':(VOTING_DRIVEN_CENSORSHIP+1),'VDP':(VOTING_DRIVEN_PIXELATION+1),\
 	'cap':caption,'av_url':av_url,'ut':upload_time,'img_url':img_url,'img_thumb':img_thumb,'is_fbs':is_fbs,'home_hash':home_hash, 'origin':origin, \
 	'show_avatar':'block' if av_url else 'none','block_time_remaining':block_time_remaining,'ooid':ooid, 'pid':pid,'raw_caption':raw_caption, \
 	'is_star':is_star,'show_uname_credentials':show_uname_credentials, 'report_display':'block' if show_report else 'none','lazyload_idx':lazyload_idx,\
 	'ex_url':True if exclude_detail_url else False, 'mt':machine_time, 'show_trending':True if show_trending == '1' else False, 'nht':nht, \
-	'ratio':ratio,'aud':aud, 'exp':exp, 'ot':ot}# a value of '6' for 'show_trending' implies the image trended
+	'ratio':ratio,'aud':aud, 'exp':exp, 'ot':ot, 'is_new':is_new}# a value of '6' for 'show_trending' implies the image trended
--- links/templatetags/get_text_object.py
+++ links/templatetags/get_text_object.py
@@ -7,10 +7,10 @@ register = template.Library()
 
 def text_object(obid, ooid, text, raw_text, origin, upload_time, machine_time, oun, av_url, vsc, static_url, home_hash=None, is_star=None, \
 	alignment=None, just_description=False, block_time_remaining=None, topic=None, topic_url=None, show_topic=None, is_fbs=False, c1=None, \
-	c2=None, aud=None, exp=None):
+	c2=None, aud=None, exp=None, is_new=None):
 	pos = 'right' if alignment == '2' else 'left'
 	context = {'obid':obid,'ooid':ooid,'text':text,'raw_text':raw_text,'origin':origin,'upload_time':upload_time,'oun':oun,'av_url':av_url,\
 	'vsc':vsc,'home_hash':home_hash,'static_url':static_url,'is_star':is_star,'VDC':VOTING_DRIVEN_CENSORSHIP,'just_description':just_description,\
 	'is_fbs':is_fbs,'block_time_remaining':block_time_remaining,'mt':machine_time,'pos':pos,'topic':topic,'topic_url':topic_url,'show_topic':show_topic,\
-	'c1':c1, 'c2':c2,'aud':aud, 'exp':exp}
+	'c1':c1, 'c2':c2,'aud':aud, 'exp':exp,'is_new':is_new}
 	return context

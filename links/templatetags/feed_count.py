d4048f56f1a92ef64861bf83c00f78c0908e87e6
mhb11 <hassan.baig@alumni.duke.edu>
2020-01-16 02:03:29 +0500

    improved 'for me' notification functionality

--- links/templatetags/feed_count.py
+++ links/templatetags/feed_count.py
@@ -1,12 +1,16 @@
-from links.redis2 import get_feed_count
+import time
 from django import template
+from links.redis2 import get_feed_count, set_for_me_seen_time
 
 register = template.Library()
 
 @register.assignment_tag(takes_context=True)
-def check_feed_count(context):
+def check_feed_count(context, is_for_me):
 	try:
 		user_id = context['request'].user.id
-		return get_feed_count(user_id) 
+		feed_count = get_feed_count(user_id)
+		if is_for_me:
+			set_for_me_seen_time(user_id, time.time())
+		return feed_count
 	except:
 		return None

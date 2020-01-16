b0a397dc2cfc346fdad1a46a6677c08bc84d3015
mhb11 <hassan.baig@alumni.duke.edu>
2020-01-16 12:20:10 +0500

    moved hell_ban check to redis (optimization)

--- unconnectedreddit/middleware/HellBanned.py
+++ unconnectedreddit/middleware/HellBanned.py
@@ -1,9 +1,10 @@
-from links.models import HellBanList
+from links.redis4 import is_user_hell_banned
+# from links.models import HellBanList
 
 class HellBannedMiddleware(object):
 
     def process_request(self, request):
-        if request.user.is_authenticated():
-        	request.user_banned = HellBanList.objects.filter(condemned=request.user).exists()
-    	else:
-    		request.user_banned = 0
+       	"""
+       	adds hell_ban status for each user
+       	"""
+        request.user_banned = is_user_hell_banned(target_id=request.user.id) if request.user.is_authenticated() else 0

dab2d4a6d104c94fad5e7a0ec5bea7a94e2a2e6d
mhb11 <hassan.baig@alumni.duke.edu>
2020-02-22 20:19:20 +0500

    added functionality that hides bottom fixed menu bar when needed

--- unconnectedreddit/static/js/module/transparent_buttons.js
+++ unconnectedreddit/static/js/module/transparent_buttons.js
@@ -529,6 +529,23 @@ function determine_pause_length(string) {
 	}
 }
 
+///////////////////////////////////////////////////////////////////////////////////
+
+var input_btns = document.getElementsByClassName('inp');// selecting all textareas or inputs for processing (e.g. making fixed bottom menu 'absolute')
+for (var i=0, len=input_btns.length; i < len; i++) {
+	input_btns[i].onfocus = () => {
+		var bottom_nav = document.getElementById("bottom_nav")
+		bottom_nav.classList.remove('show-it');
+      	bottom_nav.classList.add('hide-it');
+	}
+	input_btns[i].onblur = () => {
+		var bottom_nav = document.getElementById("bottom_nav")
+		bottom_nav.classList.remove('hide-it');
+      	bottom_nav.classList.add('show-it');
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////
 
 var report_btns = document.getElementsByClassName('report');
 for (var i=0, len=report_btns.length; i < len; i++) report_btns[i].onclick = report_modal;

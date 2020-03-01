d631cefd7076faf7c58e5f425e40b566aca3b545
mhb11 <hassan.baig@alumni.duke.edu>
2020-03-01 05:03:37 +0500

    minor tweak

--- links/forms.py
+++ links/forms.py
@@ -600,8 +600,10 @@ class SearchNicknameForm(forms.Form):
 		super(SearchNicknameForm, self).__init__(*args, **kwargs)
 		self.fields['nickname'].widget.attrs['style'] = \
 		'max-width:90%;width:500px;background-color:#F8F8F8;border: 1px solid #3cb7dd;border-radius:5px;padding: 6px 6px 6px 0;text-indent: 6px;color: #229ec9;'
-		self.fields['nickname'].widget.attrs['class'] = 'cxl'
-		if not self.searched:
+		if self.searched:
+			self.fields['nickname'].widget.attrs['class'] = 'cxl inp'
+		else:
+			self.fields['nickname'].widget.attrs['class'] = 'cxl'
 			self.fields['nickname'].widget.attrs['autofocus'] = 'autofocus'
 		self.fields['nickname'].widget.attrs['autocomplete'] = 'off'
 

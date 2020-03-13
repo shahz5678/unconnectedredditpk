8e23d88ff93cb36d16c5580237803a7b40140e0c
mhb11 <hassan.baig@alumni.duke.edu>
2020-03-13 13:45:11 +0500

    fixed a bug

--- unconnectedreddit/middleware/OperaMini.py
+++ unconnectedreddit/middleware/OperaMini.py
@@ -2,7 +2,11 @@ class OperaMiniMiddleware(object):
 
     def process_request(self, request):
     	user_agent = request.META.get('HTTP_USER_AGENT',None)
-    	if 'Presto' in user_agent and 'Opera Mini' in user_agent:
-    		request.is_opera_mini = True
-    	else:
-    		request.is_opera_mini = False
+    	if user_agent:
+	    	if 'Presto' in user_agent and 'Opera Mini' in user_agent:
+	    		request.is_opera_mini = True
+	    	else:
+	    		request.is_opera_mini = False
+	    else:
+	    	# couldn't detect user-agent at all
+	    	request.is_opera_mini = False

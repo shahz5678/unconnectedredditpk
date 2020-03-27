32c58dac637004011b5ae6a8a8c21c5383f4224c
mhb11 <hassan.baig@alumni.duke.edu>
2020-03-28 04:02:39 +0500

    added a parser that suggests alt text for images (based on their captions)

--- links/utilities.py
+++ links/utilities.py
@@ -1,7 +1,173 @@
 # coding=utf-8
-import time
+import time, uuid
 from pytz import timezone
 from datetime import datetime
+
+def image_description_generator(caption):
+	"""
+	Generates alt-text and image filename via parsing the image caption
+	Helps image SEO and screen-reader accessibility
+	"""
+	random_string = str(uuid.uuid4())# 36 chars
+	add_to_sitemap = False
+	if caption:
+		caption_lower = caption.lower()
+
+		###############################################
+		# is it a poetry related image?
+		if "poetry" in caption_lower:
+			if "funny" in caption_lower:
+				image_alt_text = 'Funny poetry'
+				image_filename = 'funny-poetry-{}'.format(random_string)
+			elif "sad " in caption_lower:
+				image_alt_text = 'Sad poetry'
+				image_filename = 'sad-poetry-{}'.format(random_string)
+			elif "love" in caption_lower:
+				image_alt_text = 'Romantic poetry'
+				image_filename = 'romantic-poetry-{}'.format(random_string)
+			elif "urdu" in caption_lower:
+				image_alt_text = 'Urdu poetry'
+				image_filename = 'urdu-poetry-{}'.format(random_string)
+			else:
+				image_alt_text = 'Poetry'
+				image_filename = 'poetry-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		# is it poetry by a certain poet?
+		elif "elia" in caption_lower or "eliya" in caption_lower:
+			if "john" in caption_lower or "jaun" in caption_lower or "jahan" in caption_lower:
+				image_alt_text = 'Jaun Elia poetry'
+				image_filename = 'jaun-elia-poetry-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it a funny meme/image?
+		elif "funny" in caption_lower or "hhhh" in caption_lower or "haha" in caption_lower \
+		or "hihi" in caption_lower or "hehe" in caption_lower or unicode("😂", "utf-8") in caption_lower \
+		or unicode("🤣", "utf-8") in caption_lower or unicode("😝", "utf-8") in caption_lower \
+		or unicode("😆", "utf-8") in caption_lower or unicode("🤪", "utf-8") in caption_lower \
+		or ":d" in caption_lower or ".a5" in caption_lower or ".c1" in caption_lower \
+		or ".c5" in caption_lower or ".b1" in caption_lower or ".b3" in caption_lower \
+		or ".d2" in caption_lower or ".d1" in caption_lower:
+			if "corona" in caption_lower:
+				image_alt_text = 'Coronavirus Funny Image'
+				image_filename = 'coronavirus-funny-{}'.format(random_string)
+			else:
+				image_alt_text = 'Funny image'
+				image_filename = 'funny-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		# is it a morning greeting?
+		elif "morning" in caption_lower or "mornng" in caption_lower \
+		or "morng" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower \
+			or "goood" in caption_lower or "gd" in caption_lower:
+				image_alt_text = 'Good morning'
+				image_filename = 'good-morning-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it an afternoon/noon greeting?
+		elif "afternoon" in caption_lower or "noon" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower \
+			or "goood" in caption_lower or "gd" in caption_lower:
+				image_alt_text = 'Good afternoon'
+				image_filename = 'good-afternoon-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it an evening greeting?
+		elif "evening" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower \
+			or "goood" in caption_lower or "gd" in caption_lower:
+				image_alt_text = 'Good evening'
+				image_filename = 'good-evening-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it a night greeting?
+		elif "night" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower \
+			or "goood" in caption_lower or "gd" in caption_lower:
+				image_alt_text = 'Good night'
+				image_filename = 'good-night-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it a holiday greeting?
+		elif "mubarak" in caption_lower or "mubarik" in caption_lower \
+		or "mobrak" in caption_lower or "mubark" in caption_lower \
+		or "mobarak" in caption_lower or "mubbark" in caption_lower \
+		or "mubaarak" in caption_lower or "mubaark" in caption_lower \
+		or "mubrk" in caption_lower or "mobarik" in caption_lower \
+		or "mobarkh" in caption_lower:
+			if "juma" in caption_lower or "jumma" in caption_lower \
+			or "jummma" in caption_lower or "joma" in caption_lower:
+				image_alt_text = 'Jumma mubarak'
+				image_filename = 'jumma-mubarak-{}'.format(random_string)
+				add_to_sitemap = True
+			elif "eid" in caption_lower:
+				image_alt_text = 'Eid mubarak'
+				image_filename = 'eid-mubarak-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = caption
+				image_filename = random_string
+
+		###############################################
+		# is it language specific?
+		elif "urdu" in caption_lower:
+			image_alt_text = 'Urdu post'
+			image_filename = 'urdu-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		# does it relate to a current issue?
+		elif "corona" in caption_lower:
+			image_alt_text = 'Coronavirus social media post'
+			image_filename = 'coronavirus-social-media-post-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		# can it be called a 'post'?
+		elif "post" in caption_lower or unicode("♥", "utf-8") in caption_lower:
+			if "islam" in caption_lower:
+				image_alt_text = 'Islamic post'
+				image_filename = 'islamic-post-{}'.format(random_string)
+			else:
+				image_alt_text = 'Social media post'
+				image_filename = 'social-media-post-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		#TODO: add more cases as the need arises
+
+		else:
+			image_alt_text = caption
+			image_filename = random_string
+	else:
+		# no image caption present for us to parse
+		image_alt_text = 'Social media image post'
+		image_filename = random_string
+
+	return image_alt_text, image_filename, add_to_sitemap
 
 
 def beautiful_date(epoch_time, format_type='1'):

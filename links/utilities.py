a759770dd369ce620ec1510146008abdef5ca70c
mhb11 <hassan.baig@alumni.duke.edu>
2020-03-29 14:25:09 +0500

    tweaked

--- links/utilities.py
+++ links/utilities.py
@@ -36,13 +36,26 @@ def image_description_generator(caption):
 		###############################################
 		# is it poetry by a certain poet?
 		elif "elia" in caption_lower or "eliya" in caption_lower:
-			if "john" in caption_lower or "jaun" in caption_lower or "jahan" in caption_lower or "joun" in caption_lower:
+			if "john" in caption_lower or "jaun" in caption_lower or "jahan" in caption_lower or "joun" in caption_lower \
+			or "joon" in caption_lower or "jon" in caption_lower:
 				image_alt_text = 'Jaun Elia poetry'
 				image_filename = 'jaun-elia-poetry-{}'.format(random_string)
 				add_to_sitemap = True
 			else:
 				image_alt_text = caption
 				image_filename = random_string
+
+		###############################################
+		# is it birthday related?
+		elif "birthday" in caption_lower or "brithday" in caption_lower or "brthday" in caption_lower or "brday" in caption_lower:
+			if "happy" in caption_lower:
+				image_alt_text = 'Happy birthday'
+				image_filename = 'happy-birthday-{}'.format(random_string)
+				add_to_sitemap = True
+			else:
+				image_alt_text = 'Birthday'
+				image_filename = 'birthday-{}'.format(random_string)
+				add_to_sitemap = True
 
 		###############################################
 		# is it a funny meme/image?
@@ -53,21 +66,26 @@ def image_description_generator(caption):
 		or unicode("😁", "utf-8") in caption_lower or unicode("😅", "utf-8") in caption_lower \
 		or ":d" in caption_lower or ".a5" in caption_lower or ".c1" in caption_lower \
 		or ".c5" in caption_lower or ".b1" in caption_lower or ".b3" in caption_lower \
-		or ".d2" in caption_lower or ".d1" in caption_lower:
+		or ".d2" in caption_lower or ".d1" in caption_lower or ".d3" in caption_lower:
 			if "corona" in caption_lower or "carona" in caption_lower or "crona" in caption_lower or "caroona" in caption_lower:
 				image_alt_text = 'Corona meme, carona meme, coronavirus meme, corona joke'
 				image_filename = 'coronavirus-funny-{}'.format(random_string)
+			elif "lock" in caption_lower and "down" in caption_lower:
+				image_alt_text = 'Lockdown meme, lockdown funny, lockdown joke'
+				image_filename = 'lockdown-funny-{}'.format(random_string)
+			elif "quarantine" in caption_lower:
+				image_alt_text = 'Quarantine meme, quarantine funny, quarantine joke'
+				image_filename = 'quarantine-funny-{}'.format(random_string)
 			else:
 				image_alt_text = 'Funny joke'
-				image_filename = 'funny-{}'.format(random_string)
+				image_filename = 'funny-joke-{}'.format(random_string)
 			add_to_sitemap = True
 
 		###############################################
 		# is it a morning greeting?
-		elif "morning" in caption_lower or "mornng" in caption_lower \
-		or "morng" in caption_lower:
-			if "good" in caption_lower or "gud" in caption_lower \
-			or "goood" in caption_lower or "gd" in caption_lower \
+		elif "morning" in caption_lower or "mornng" in caption_lower or "morng" in caption_lower or "mr9g" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower or "goood" in caption_lower \
+			or "gooood" in caption_lower or "gd" in caption_lower or "g00d" in caption_lower \
 			or ".q1" in caption_lower:
 				image_alt_text = 'Good morning'
 				image_filename = 'good-morning-{}'.format(random_string)
@@ -79,7 +97,7 @@ def image_description_generator(caption):
 		###############################################
 		# is it a "subha bakhair" greeting?
 		elif "bakhair" in caption_lower or "bakhir" in caption_lower or "bakher" in caption_lower or "baker" in caption_lower:
-			if "subha" in caption_lower or "suba" in caption_lower:
+			if "subha" in caption_lower or "suba" in caption_lower or "soba" in caption_lower:
 				image_alt_text = 'Subha bakhair'
 				image_filename = 'subha-bakhair-{}'.format(random_string)
 				add_to_sitemap = True
@@ -90,8 +108,8 @@ def image_description_generator(caption):
 		###############################################
 		# is it an afternoon/noon greeting?
 		elif "afternoon" in caption_lower or "noon" in caption_lower:
-			if "good" in caption_lower or "gud" in caption_lower \
-			or "goood" in caption_lower or "gd" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower or "goood" in caption_lower \
+			or "gooood" in caption_lower or "gd" in caption_lower:
 				image_alt_text = 'Good afternoon'
 				image_filename = 'good-afternoon-{}'.format(random_string)
 				add_to_sitemap = True
@@ -102,8 +120,8 @@ def image_description_generator(caption):
 		###############################################
 		# is it an evening greeting?
 		elif "evening" in caption_lower:
-			if "good" in caption_lower or "gud" in caption_lower \
-			or "goood" in caption_lower or "gd" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower or "goood" in caption_lower \
+			or "gooood" in caption_lower or "gd" in caption_lower:
 				image_alt_text = 'Good evening'
 				image_filename = 'good-evening-{}'.format(random_string)
 				add_to_sitemap = True
@@ -114,8 +132,8 @@ def image_description_generator(caption):
 		###############################################
 		# is it a night greeting?
 		elif "night" in caption_lower:
-			if "good" in caption_lower or "gud" in caption_lower \
-			or "goood" in caption_lower or "gd" in caption_lower:
+			if "good" in caption_lower or "gud" in caption_lower or "goood" in caption_lower \
+			or "gooood" in caption_lower or "gd" in caption_lower:
 				image_alt_text = 'Good night'
 				image_filename = 'good-night-{}'.format(random_string)
 				add_to_sitemap = True
@@ -125,14 +143,11 @@ def image_description_generator(caption):
 
 		###############################################
 		# is it a holiday greeting?
-		elif "mubarak" in caption_lower or "mubarik" in caption_lower \
-		or "mobrak" in caption_lower or "mubark" in caption_lower \
-		or "mobarak" in caption_lower or "mubbark" in caption_lower \
-		or "mubaarak" in caption_lower or "mubaark" in caption_lower \
-		or "mubrk" in caption_lower or "mobarik" in caption_lower \
-		or "mobarkh" in caption_lower:
-			if "juma" in caption_lower or "jumma" in caption_lower \
-			or "jummma" in caption_lower or "joma" in caption_lower:
+		elif "mubarak" in caption_lower or "mubarik" in caption_lower or "mobrak" in caption_lower \
+		or "mubark" in caption_lower or "mobarak" in caption_lower or "mubbark" in caption_lower \
+		or "mubaarak" in caption_lower or "mubaark" in caption_lower or "mubrk" in caption_lower \
+		or "mobarik" in caption_lower or "mobarkh" in caption_lower:
+			if "juma" in caption_lower or "jumma" in caption_lower or "jummma" in caption_lower or "joma" in caption_lower:
 				image_alt_text = 'Jumma mubarak'
 				image_filename = 'jumma-mubarak-{}'.format(random_string)
 				add_to_sitemap = True
@@ -154,14 +169,14 @@ def image_description_generator(caption):
 		###############################################
 		# does it relate to a current issue?
 		elif "corona" in caption_lower or "carona" in caption_lower or "caroona" in caption_lower:
-			image_alt_text = 'Corona social media post, carona social media post'
+			image_alt_text = 'Corona social media post, carona social media post, coronavirus post'
 			image_filename = 'coronavirus-social-media-post-{}'.format(random_string)
 			add_to_sitemap = True
 
 		###############################################
 		# can it be called a 'post'?
-		elif "post" in caption_lower or unicode("♥", "utf-8") in caption_lower or unicode("❤️","utf-8") in caption_lower:
-			if "islam" in caption_lower:
+		elif "post" in caption_lower or unicode("♥", "utf-8") in caption_lower:
+			if "islam " in caption_lower or "islamic" in caption_lower:
 				image_alt_text = 'Islamic post'
 				image_filename = 'islamic-post-{}'.format(random_string)
 			else:
@@ -170,15 +185,39 @@ def image_description_generator(caption):
 			add_to_sitemap = True
 
 		###############################################
-		# is it language specific?
-		elif "just" in caption_lower:
-			if "fun" in caption_lower:
-				image_alt_text = 'Funny post'
+		# is it 'just fun' or 'for fun'?
+		elif "fun" in caption_lower:
+			if "just" in caption_lower or "for" in caption_lower:
+				image_alt_text = 'Funny post, fun post'
 				image_filename = 'funny-post-{}'.format(random_string)
 				add_to_sitemap = True
 			else:
 				image_alt_text = caption
 				image_filename = random_string
+
+		###############################################
+		# is it a motivational quote or "golden words" post?
+		elif "motivation" in caption_lower:
+			image_alt_text = 'Inspirational quote, success quote, motivational quote, motivation quote'
+			image_filename = 'inspirational-quote-{}'.format(random_string)
+			add_to_sitemap = True
+
+		elif "golden words" in caption_lower:
+			image_alt_text = 'Golden words'
+			image_filename = 'golden-words-{}'.format(random_string)
+			add_to_sitemap = True
+
+		###############################################
+		# is it quarantine or lockdown specific?
+		elif "quarantine" in caption_lower:
+			image_alt_text = 'Quarantine post, quarantine social media post'
+			image_filename = 'quarantine-post-{}'.format(random_string)
+			add_to_sitemap = True
+
+		elif "lockdown" in caption_lower:
+			image_alt_text = 'Lockdown post, lockdown social media post'
+			image_filename = 'lockdown-post-{}'.format(random_string)
+			add_to_sitemap = True
 
 		###############################################
 		#TODO: add more cases as the need arises
